ba964afbc1d5cc2b6aba768652a2b51d8420f0cf
sakuya9t <sakuya9t@vip.qq.com>
2018-11-24 17:17:22 +1100

    basic insertion added

--- app/webservice/FirebaseAPI.py
+++ app/webservice/FirebaseAPI.py
@@ -7,9 +7,17 @@ class filebaseAPI:
         self.app = firebase.FirebaseApplication('https://summer-hackathon-84d96.firebaseio.com/', None)
         authentication = FirebaseAuthentication(key, mailbox, True, True)
         self.app.authentication = authentication
+        self.token = authentication.get_user().firebase_auth_token
+        print(self.token)
 
-    def query(self, table_name, filter):
-
+    def query(self, database_name, table_name):
+        params = {"print": "pretty"}
+        result = self.app.get_async(database_name, table_name, params)
+        return result
 
     def insert(self, database_name, table_name, data, callback_func):
         self.app.put_async(database_name, table_name, data, callback=callback_func)
+
+
+if __name__ == "__main__":
+    app = filebaseAPI("oin3wrNHY1tBS9uj6EScGcdtwpZZhnT1KMt2XSX6", "wangzijian09@gmail.com")
--- app/webservice/firebase_test.py
+++ app/webservice/firebase_test.py
@@ -1,16 +1,26 @@
+import json
+
 from firebase import firebase
 from firebase.firebase import FirebaseAuthentication
 
 app = firebase.FirebaseApplication('https://summer-hackathon-84d96.firebaseio.com/', None)
 
 
-authentication = FirebaseAuthentication("yourkey", "youremail", True, True)
+authentication = FirebaseAuthentication("oin3wrNHY1tBS9uj6EScGcdtwpZZhnT1KMt2XSX6", "wangzijian09@gmail.com", True, True)
 app.authentication = authentication
 
 user = authentication.get_user()
 
-result = app.get('/users', None, params={'print': 'pretty'})
-
+result = app.get('/tests', name="specs",
+                 params={"orderBy": '"prompt"', "startAt": '"Are"', "endAt": '"Area"', "print": "pretty"})
+print("test")
 print(result)
 
-data={"abc":"def", "agg": 2}
+# input_file = open('rules.json')
+# # input_file = open('../../spec/spec-example.json')
+# json_array = json.load(input_file)
+# print(json_array)
+
+# data={"abc":"def", "agg": 2}
+#
+# app.put("/", "rules", json_array)
