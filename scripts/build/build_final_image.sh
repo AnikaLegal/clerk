05845fc3b770718be534f02a4208580058bfc91e
Matthew Segal <mattdsegal@gmail.com>
2018-11-30 12:19:14 +1100

    add build folder

---
+++ scripts/build/build_final_image.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+set -e
+
+# Build final docker image and push it to Docker Hub
+DOCKER_ID='anikalaw'
+IMAGE_NAME='clerk'
+DOCKERFILE='Dockerfile'
+
+cat ~/.dockercreds | docker login --username $DOCKER_ID --password-stdin
+docker build -t $DOCKER_ID/$IMAGE_NAME:latest -f $DOCKERFILE .
+docker push $DOCKER_ID/$IMAGE_NAME
