bf6ae5f046680ac43683e8304c2aa8170a400e8d
Matthew Segal <mattdsegal@gmail.com>
2019-05-28 21:26:15 +1000

    Finish file upload

--- app/questions/services/submission.py
+++ app/questions/services/submission.py
@@ -1,8 +1,8 @@
 import logging
 
 from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 
-from .mailer import Email
 from questions.models import ImageUpload
 
 logger = logging.getLogger(__name__)
@@ -13,7 +13,6 @@ def send_submission_email(submission):
     Send a submission alert email with data included.
     """
     subject = f"Client intake submission {str(submission.id)}"
-    attached_files = []
     if settings.DEBUG:
         subject = "[TEST] " + subject
 
@@ -24,21 +23,24 @@ def send_submission_email(submission):
         logger.error("Submission[%s] has insufficient data", submission.id)
 
     # Assume answer is a list of dicts with a pretty specific structure.
+    images = []
     for answer in answers:
         question = questions[answer["name"]]
         if question["type"] == "FILE":
             ids = [i["id"] for i in answer["answer"]]
-            attached_files += [
-                i.image.file.file for i in ImageUpload.objects.filter(pk__in=ids).all()
-            ]
+            images += [i.image for i in ImageUpload.objects.filter(pk__in=ids).all()]
         else:
             body += question["name"] + " " + question.get("prompt", "NO PROMPT") + "\n\n"
             body += repr(answer["answer"]) + "\n\n"
 
     logger.info("Sending email for Submission[%s]", submission.id)
-    (
-        Email(subject=subject, recipients=settings.SUBMISSION_EMAILS)
-        .with_text_body(body)
-        .with_files(attached_files)
-        .send()
+    email = EmailMultiAlternatives(
+        subject=subject,
+        body=body,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=settings.SUBMISSION_EMAILS,
     )
+    for image in images:
+        email.attach(image.name.split("/")[-1], image.read())
+
+    email.send(fail_silently=False)
