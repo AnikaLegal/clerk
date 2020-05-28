7b395e1b919ba1d896d939ce7e210b40e2f58c50
Matthew Segal <mattdsegal@gmail.com>
2020-05-28 17:16:15 +1000

    Add streamlit demo

--- app/clerk/urls.py
+++ app/clerk/urls.py
@@ -5,12 +5,15 @@ from rest_framework import routers
 
 from questions.views import apis
 from webhooks.views import webflow_form_view
+from .views import reports_view
+
 
 router = routers.SimpleRouter()
 router.register("submission", apis.SubmissionViewSet, basename="submission")
 router.register("images", apis.ImageUploadViewSet, basename="images")
 router.register("files", apis.FileUploadViewSet, basename="files")
 urlpatterns = [
+    path("reports/", reports_view, name="reports"),
     path("admin/", admin.site.urls, name="admin"),
     path("api/webhooks/webflow-form/", webflow_form_view, name="webflow-form"),
     path("api/", include(router.urls)),
---
+++ app/clerk/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url="/admin/login/")
+def reports_view(request):
+    """
+    Internal redirect to Streamlit app.
+    See https://wellfire.co/learn/nginx-django-x-accel-redirects/
+    """
+    response = HttpResponse()
+    response["X-Accel-Redirect"] = "/streamlit/"
+    return response
