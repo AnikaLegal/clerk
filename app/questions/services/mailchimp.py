fa722e3dd4352c40409ad1560fba740fe8753691
Peter Zhang <lpzhang@me.com>
2020-07-31 13:18:43 +1000

    Refactor mailchimp functions

--- app/questions/services/mailchimp.py
+++ app/questions/services/mailchimp.py
@@ -7,53 +7,59 @@ from questions.models import Submission
 
 
 def remind_incomplete():
-    """master function finding clients and sending reminder emails to them"""
+    """Master function finding clients and sending reminder emails to them"""
 
-    # find COVID clients and send reminder
-    covid_clients = find_clients(topic='COVID')
-    send_email(clients=covid_clients, list_id='', workflow_id='', email_id='')
+    # Find COVID clients and send reminder
+    covid_clients = find_clients(topic="COVID")
+    send_email(clients=covid_clients, list_id="", workflow_id="", email_id="")
 
-    # find REPAIRS clients and send reminder
-    repairs_clients = find_clients(topic='REPAIRS')
-    send_email(clients=repairs_clients, list_id='', workflow_id='', email_id='')
+    # Find REPAIRS clients and send reminder
+    repairs_clients = find_clients(topic="REPAIRS")
+    send_email(clients=repairs_clients, list_id="", workflow_id="", email_id="")
 
 
 def find_clients(topic):
-    """find and return list of clients according to business criteria"""
-    
-    # define the boundaries of our time range
+    """Find and return list of clients according to business criteria"""
+
+    # Define the boundaries of our time range
     two_days_ago = timezone.now() - timezone.timedelta(days=2)
     two_weeks_ago = timezone.now() - timezone.timedelta(days=14)
 
-    # find submissions according to conditions specified
-    submissions = Submission.objects.filter(complete=False, is_reminder_sent=False, topic=topic,
-                            created_at__gt=two_weeks_ago, created_at__lt=two_days_ago)
-    
-    # generate list of clients with emails inside of submissions
+    # Find submissions according to conditions specified
+    submissions = Submission.objects.filter(
+        complete=False,
+        is_reminder_sent=False,
+        topic=topic,
+        created_at__gt=two_weeks_ago,
+        created_at__lt=two_days_ago,
+    )
+
+    # Generate list of clients with emails inside of submissions
     clients = []
 
     for submission in submissions:
         for answer in submission.answers:
-            if answer["name"] == 'CLIENT_EMAIL' and answer["answer"]:
+            if answer["name"] == "CLIENT_EMAIL" and answer["answer"]:
                 client = (submission, answer["answer"])
                 clients.append(client)
-    
+
     return clients
 
-            
+
 def send_email(clients, list_id, workflow_id, email_id):
-    """send reminder email via MailChimp API"""
-    
+    """Send reminder email via MailChimp API"""
+
     mailchimp = MailChimp(settings.MAILCHIMP_API_KEY)
 
     for submission, email in clients:
-        # mark as sent reminder
+        # Add formatting
+        person = {"email_address": email, "status": "subscribed"}
+        # Add person to list
+        mailchimp.lists.members.create(list_id=list_id, data=person)
+        # Send person email
+        mailchimp.automations.emails.queues.create(
+            workflow_id=workflow_id, email_id=email_id, data=person
+        )
+        # Mark as sent
         submission.is_reminder_sent = True
         submission.save()
-        # add formatting
-        person = {'email_address': email, 'status': 'subscribed'}
-        # add person to list
-        mailchimp.lists.members.create(list_id=list_id, data=person)
-        # send person email
-        mailchimp.automations.emails.queues.create(workflow_id=workflow_id,
-                            email_id=email_id, data=person)
--- app/questions/tests/test_reminder_email.py
+++ app/questions/tests/test_reminder_email.py
@@ -2,26 +2,29 @@ from django.utils import timezone
 
 from unittest import mock
 import pytest
-import factory
 
-from questions.services.mailchimp import remind_incomplete, find_clients, send_email
+from questions.services.mailchimp import remind_incomplete, find_clients
 from questions.tests.factories import SubmissionFactory
 from questions.models import Submission
 
 
-# sample answers for testing
-answers_with_email = [{'name': 'CLIENT_NAME', 'answer': 'Michael Jordan'},
-{'name': 'CLIENT_RENTAL_ADDRESS', 'answer': 'Chicago Bulls'},
-{'name': 'CLIENT_DOB', 'answer': '1963-02-17'},
-{'name': 'CLIENT_EMAIL', 'answer': 'michael@jordan.com'},
-{'name': 'CLIENT_PHONE', 'answer': '4458 4858'}]
+# Sample answers for testing
+answers_with_email = [
+    {"name": "CLIENT_NAME", "answer": "Michael Jordan"},
+    {"name": "CLIENT_RENTAL_ADDRESS", "answer": "Chicago Bulls"},
+    {"name": "CLIENT_DOB", "answer": "1963-02-17"},
+    {"name": "CLIENT_EMAIL", "answer": "michael@jordan.com"},
+    {"name": "CLIENT_PHONE", "answer": "4458 4858"},
+]
 
-answers_no_email = [{'name': 'CLIENT_NAME', 'answer': 'Paul Scholes'},
-{'name': 'CLIENT_RENTAL_ADDRESS', 'answer': 'Manchester United'},
-{'name': 'CLIENT_DOB', 'answer': '1974-11-16'},
-{'name': 'CLIENT_PHONE', 'answer': '4567 4756'}]
+answers_no_email = [
+    {"name": "CLIENT_NAME", "answer": "Paul Scholes"},
+    {"name": "CLIENT_RENTAL_ADDRESS", "answer": "Manchester United"},
+    {"name": "CLIENT_DOB", "answer": "1974-11-16"},
+    {"name": "CLIENT_PHONE", "answer": "4567 4756"},
+]
 
-# sample dates for testing
+# Sample dates for testing
 three_weeks_ago = timezone.now() - timezone.timedelta(days=21)
 three_days_ago = timezone.now() - timezone.timedelta(days=3)
 one_day_ago = timezone.now() - timezone.timedelta(days=1)
@@ -29,47 +32,87 @@ one_day_ago = timezone.now() - timezone.timedelta(days=1)
 
 @pytest.mark.django_db
 def test_find_clients_no_emails():
-    sub1 = SubmissionFactory(complete=False, is_reminder_sent=False, topic="COVID",
-    created_at=three_days_ago, answers=answers_no_email)
+    sub1 = SubmissionFactory(
+        complete=False,
+        is_reminder_sent=False,
+        topic="COVID",
+        created_at=three_days_ago,
+        answers=answers_no_email,
+    )
     results = find_clients("COVID")
     assert results == []
 
 
 @pytest.mark.django_db
 def test_find_clients_with_emails():
-    sub1 = SubmissionFactory(complete=False, is_reminder_sent=False, topic="REPAIRS",
-    created_at=three_days_ago, answers=answers_no_email)
-    sub2 = SubmissionFactory(complete=False, is_reminder_sent=False, topic="REPAIRS",
-    created_at=three_days_ago, answers=answers_with_email)
+    sub1 = SubmissionFactory(
+        complete=False,
+        is_reminder_sent=False,
+        topic="REPAIRS",
+        created_at=three_days_ago,
+        answers=answers_no_email,
+    )
+    sub2 = SubmissionFactory(
+        complete=False,
+        is_reminder_sent=False,
+        topic="REPAIRS",
+        created_at=three_days_ago,
+        answers=answers_with_email,
+    )
     results = find_clients("REPAIRS")
     assert len(results) == 1
 
 
 @pytest.mark.django_db
 def test_find_clients_irrelevant_dates():
-    sub1 = SubmissionFactory(complete=False, is_reminder_sent=False, topic="COVID",
-    created_at=three_weeks_ago, answers=answers_with_email)
-    sub2 = SubmissionFactory(complete=False, is_reminder_sent=False, topic="COVID",
-    created_at=one_day_ago, answers=answers_with_email)
+    sub1 = SubmissionFactory(
+        complete=False,
+        is_reminder_sent=False,
+        topic="COVID",
+        created_at=three_weeks_ago,
+        answers=answers_with_email,
+    )
+    sub2 = SubmissionFactory(
+        complete=False,
+        is_reminder_sent=False,
+        topic="COVID",
+        created_at=one_day_ago,
+        answers=answers_with_email,
+    )
     results = find_clients("COVID")
     assert results == []
 
 
 @pytest.mark.django_db
 def test_find_clients_relevant_dates():
-    sub1 = SubmissionFactory(complete=False, is_reminder_sent=False, topic="REPAIRS",
-    created_at=three_weeks_ago, answers=answers_with_email)
-    sub2 = SubmissionFactory(complete=False, is_reminder_sent=False, topic="REPAIRS",
-    created_at=three_days_ago, answers=answers_with_email)
+    sub1 = SubmissionFactory(
+        complete=False,
+        is_reminder_sent=False,
+        topic="REPAIRS",
+        created_at=three_weeks_ago,
+        answers=answers_with_email,
+    )
+    sub2 = SubmissionFactory(
+        complete=False,
+        is_reminder_sent=False,
+        topic="REPAIRS",
+        created_at=three_days_ago,
+        answers=answers_with_email,
+    )
     results = find_clients("REPAIRS")
     assert len(results) == 1
 
 
 @mock.patch("questions.services.mailchimp.MailChimp")
 @pytest.mark.django_db
-def test_remind_incomplete_with_email(mock_API):
-    sub1 = SubmissionFactory(complete=False, is_reminder_sent=False, topic="COVID",
-    created_at=three_days_ago, answers=answers_with_email)
+def test_remind_incomplete_sent(mock_API):
+    sub1 = SubmissionFactory(
+        complete=False,
+        is_reminder_sent=False,
+        topic="COVID",
+        created_at=three_days_ago,
+        answers=answers_with_email,
+    )
     remind_incomplete()
     sub1 = Submission.objects.get(pk=sub1.id)
     assert sub1.is_reminder_sent == True
