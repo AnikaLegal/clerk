5dde1b1593f257d0812db8e0d723d18af51f7bd9
Matthew Segal <mattdsegal@gmail.com>
2020-10-09 11:12:45 +1100

    Try fix deployment script

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -3,27 +3,49 @@
 # Deploy clerk the host server
 # Run this from the project root
 #
+set -e
 HOST='3.106.55.74'
 
-# Ensure SSH is set up
+if [[ -z "$COMPOSE_SUFFIX" ]]
+then
+    echo -e "\n>>> Error: Docker Compose suffix not found in COMPOSE_SUFFIX."
+    exit 1
+fi
 
-mkdir -p ~/.ssh
-echo "$CLERK_PRIVATE_SSH_KEY" > ~/.ssh/clerk-deploy
+if [[ -z "$PROJECT" ]]
+then
+    echo -e "\n>>> Error: Project name not found in PROJECT."
+    exit 1
+fi
 
+if [[ -z "$CLERK_PRIVATE_SSH_KEY" ]]
+then
+    echo -e "\n>>> Error: Clerk private key not found in CLERK_PRIVATE_SSH_KEY."
+    exit 1
+fi
 
-ssh -i ~/.ssh/clerk-deploy root@$HOST /bin/bash << EOF
+echo -e "\n>>> Deploying $PROJECT to clerk at $HOST with suffix $COMPOSE_SUFFIX."
+
+# Set up SSH key
+echo -e "\n>>> Setting up private key."
+echo -e "$CLERK_PRIVATE_SSH_KEY" > deploy.key
+
+echo -e "\n>>> SSHing into clerk at $HOST."
+ssh -i deploy.key root@$HOST /bin/bash << EOF
     set -e
-    echo "Setting up deployment files for $PROJECT"
+    echo -e "\n>>> Setting up deployment files for $PROJECT"
     mkdir -p /srv/${PROJECT}/
 EOF
 
-echo "Copying $PROJECT compose file"
+echo -e "\n>>> Copying $PROJECT compose file to clerk at $HOST"
 scp docker/docker-compose.${COMPOSE_SUFFIX}.yml root@${HOST}:/srv/$PROJECT
 
-ssh -i ~/.ssh/clerk-deploy root@$HOST /bin/bash << EOF
+echo -e "\n>>> SSHing into clerk at $HOST."
+ssh -i deploy.key root@$HOST /bin/bash << EOF
     set -e
-    echo "Deploying $PROJECT to docker swarm"
+    echo -e "\n>>> Deploying $PROJECT to Docker Swarm cluster"
     docker stack deploy \
         --compose-file /srv/${PROJECT}/docker-compose.${COMPOSE_SUFFIX}.yml \
         $PROJECT
 EOF
+echo -e "\n>>> Deployment finished for $PROJECT"
