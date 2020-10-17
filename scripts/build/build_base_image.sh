729009209c7cbc571a5d53f3a6da10b5f5a26067
Matthew Segal <mattdsegal@gmail.com>
2020-10-18 08:12:45 +1100

    Rework base Dockerfile and add playwright

--- scripts/build/build_base_image.sh
+++ scripts/build/build_base_image.sh
@@ -1,11 +1,17 @@
 #/bin/bash
 set -e
 
+if [ -z "$DOCKER_PASSWORD" ]
+then
+    echo "DOCKER_PASSWORD not set."
+    exit 1
+fi
+
 # Build base docker image and push it to Docker Hub
 DOCKER_ID='anikalaw'
 IMAGE_NAME='clerkbase'
 DOCKERFILE='docker/Dockerfile.base'
 
-cat ~/.dockercreds | docker login --username $DOCKER_ID --password-stdin
+docker login --username $DOCKER_ID --password $DOCKER_PASSWORD
 docker build -t $DOCKER_ID/$IMAGE_NAME:latest -f $DOCKERFILE .
 docker push $DOCKER_ID/$IMAGE_NAME
