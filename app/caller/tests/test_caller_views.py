cb46128f585ecd79308a52b553dcec4f6f6ed580
Peter Zhang <lpzhang@me.com>
2020-12-08 20:23:54 +1100

    Write tests for call centre

---
+++ app/caller/tests/test_caller_views.py
@@ -0,0 +1,70 @@
+from unittest import mock
+import pytest
+
+from django.urls import reverse
+
+from caller.models import Call
+
+
+@mock.patch("caller.views.MessagingResponse")
+def test_message_view(mock_MR, client):
+    """
+    User can send SMS to Twilio number.
+    """
+    mock_response = mock.Mock()
+    mock_MR.return_value = mock_response
+
+    resp = client.get(reverse("caller-message"))
+
+    mock_response.message.assert_called_once()
+    assert resp.status_code == 200
+
+
+@mock.patch("caller.views.Gather")
+@mock.patch("caller.views.VoiceResponse")
+@pytest.mark.django_db
+def test_answer_view(mock_VR, mock_G, client):
+    """
+    User can call Twilio number.
+    """
+    mock_response = mock.Mock()
+    mock_VR.return_value = mock_response
+    mock_gather = mock.Mock()
+    mock_G.return_value = mock_gather
+    from_number = "+61456654377"
+
+    resp = client.get(reverse("caller-answer"), {"From": from_number})
+
+    mock_response.append.assert_called_once()
+    mock_gather.play.assert_called_once()
+    call = Call.objects.last()
+    assert call.phone_number == from_number
+    assert resp.status_code == 200
+
+
+@mock.patch("caller.views.Client")
+@mock.patch("caller.views.Gather")
+@mock.patch("caller.views.VoiceResponse")
+@pytest.mark.django_db
+def test_collect_view(mock_VR, mock_G, mock_C, client):
+    """
+    User can select choice.
+    """
+    mock_response = mock.Mock()
+    mock_VR.return_value = mock_response
+    mock_gather = mock.Mock()
+    mock_G.return_value = mock_gather
+    mock_client = mock.Mock()
+    mock_C.return_value = mock_client
+    from_number = "+61456654377"
+    choice = "3"
+
+    client.get(reverse("caller-answer"), {"From": from_number})
+    resp = client.get(
+        reverse("caller-collect"), {"From": from_number, "Digits": choice}
+    )
+
+    mock_client.messages.create.assert_called_once()
+    call = Call.objects.last()
+    assert call.phone_number == from_number and call.requires_callback == True
+    assert resp.status_code == 200
