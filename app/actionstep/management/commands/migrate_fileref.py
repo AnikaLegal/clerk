66cc936b40342f3663d7085c12d44890cce5aa4d
Matthew Segal <mattdsegal@gmail.com>
2020-12-11 14:04:16 +1100

    Improve fileref sync from ActionStep

--- app/actionstep/management/commands/migrate_fileref.py
+++ app/actionstep/management/commands/migrate_fileref.py
@@ -3,7 +3,7 @@ import logging
 from django.core.management.base import BaseCommand
 
 from core.models import Issue
-from actionstep.api import ActionstepAPI
+from actionstep.api import ActionstepAPI, participants
 
 
 logger = logging.getLogger(__name__)
@@ -14,9 +14,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         api = ActionstepAPI()
+        actions = api.actions.list()
 
-        for issue in Issue.objects.filter(is_case_sent=True).all():
+        for issue in Issue.objects.select_related("client").all():
             # Check if this issue already has an action
+            if issue.fileref:
+                logger.info("Issue<%s> already has fileref %s", issue.id, issue.fileref)
+                continue
 
             logger.info("Checking fileref for Issue<%s>", issue.id)
             action_id = None
@@ -30,6 +34,48 @@ class Command(BaseCommand):
                 action_data = api.actions.get(action_id)
                 fileref_name = action_data["reference"]
                 logger.info("Setting fileref %s for Issue<%s>", fileref_name, issue.pk)
-                Issue.objects.filter(pk=issue.pk).update(fileref=fileref_name)
+                Issue.objects.filter(pk=issue.pk).update(
+                    fileref=fileref_name, is_case_sent=True
+                )
             else:
-                logger.info("Found existing matter %s for %s", action_id, issue.pk)
+                logger.info(
+                    "Could not find a matter Issue<%s> based on pk, trying client email.",
+                    issue.pk,
+                )
+                client = issue.client
+                participant = api.participants.get_by_email(client.email)
+                if participant:
+                    participant_id = str(participant["id"])
+                    participant_actions = [
+                        a
+                        for a in actions
+                        if participant_id in a["links"]["primaryParticipants"]
+                    ]
+                    assert (
+                        len(participant_actions) < 2
+                        or participant_actions[0]["reference"] == "C0075"
+                    )
+                    if participant_actions:
+                        action_data = participant_actions[0]
+                        logger.info(
+                            "Found existing matter %s for %s",
+                            action_data["id"],
+                            issue.pk,
+                        )
+                        fileref_name = action_data["reference"]
+                        logger.info(
+                            "Setting fileref %s for Issue<%s>", fileref_name, issue.pk
+                        )
+                        Issue.objects.filter(pk=issue.pk).update(
+                            fileref=fileref_name, is_case_sent=True
+                        )
+                    else:
+                        logger.info(
+                            "Could not find a matter Issue<%s> based on participant.",
+                            issue.pk,
+                        )
+                else:
+                    logger.info(
+                        "Could not find a matter Issue<%s> based on participant.",
+                        issue.pk,
+                    )
