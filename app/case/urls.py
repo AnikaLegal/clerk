25a2478a2e9fb62c3c16ec9e4a1806aabdccaaee
Matt Segal <mattdsegal@gmail.com>
2021-04-01 14:56:41 +1100

    Add Google OAuth2 login (#44)

--- app/case/urls.py
+++ app/case/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path("robots.txt", views.RobotsView.as_view()),
-    path("", views.ExampleView.as_view()),
+    # Auth
+    path("oauth/", include("social_django.urls", namespace="social")),
+    path("login/", views.LoginView.as_view(), name="login"),
+    # Example
+    path("", views.ExampleView.as_view(), name="example"),
 ]
