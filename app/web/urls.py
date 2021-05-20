fdb4cfc539ca036f35b4a3d905fc61a92b503891
Matthew Segal <mattdsegal@gmail.com>
2021-05-20 16:03:16 +1000

    Add more images to website

--- app/web/urls.py
+++ app/web/urls.py
@@ -8,31 +8,62 @@ def template(name):
     return TemplateView.as_view(template_name=name)
 
 
-# Views where the path, name and template name are the same.
-repeated_names = [
-    "terms-of-use",
-    "eligibility-criteria",
-    "privacy-policy",
-    "collections-statement",
-]
-
-repeated_paths = [
-    path(f"{n}/", template(f"web/{n}.html"), name=n) for n in repeated_names
-]
-
-
-urlpatterns = repeated_paths + [
-    path("about/", template("web/about.html"), name="about"),
-    path("about/team/", views.team_view, name="team"),
-    path("about/impact/", template("web/impact.html"), name="impact"),
-    path("about/partners/", template("web/partners.html"), name="partners"),
-    path("services/community/", template("web/community.html"), name="community"),
-    path("services/rental-repairs/", template("web/repairs.html"), name="repairs"),
+urlpatterns = [
     path(
-        "services/eviction-support/", template("web/evictions.html"), name="evictions"
+        "terms-of-use/", template("web/legalese/terms-of-use.html"), name="terms-of-use"
     ),
-    # TODO: Blog and blog articles (by slug)
-    # path("/blog/", ???, name="blog"),
+    path(
+        "eligibility-criteria/",
+        template("web/legalese/eligibility-criteria.html"),
+        name="eligibility-criteria",
+    ),
+    path(
+        "privacy-policy/",
+        template("web/legalese/privacy-policy.html"),
+        name="privacy-policy",
+    ),
+    path(
+        "collections-statement/",
+        template("web/legalese/collections-statement.html"),
+        name="collections-statement",
+    ),
+    # path(
+    #     "partnerships/philanthropy",
+    #     template("web/philanthropy.html"),
+    #     name="philanthropy",
+    # ),
+    # path("partnerships/corporates", template("web/corporates.html"), name="corporates"),
+    # path(
+    #     "partnerships/universities",
+    #     template("web/universities.html"),
+    #     name="universities",
+    # ),
+    # path(
+    #     "partnerships/community",
+    #     template("web/partnerships/community.html"),
+    #     name="community",
+    # ),
+    path(
+        "resources/community",
+        template("web/resources/community.html"),
+        name="refer",
+    ),
+    path("openings/", template("web/openings.html"), name="openings"),
+    path("about/", template("web/about/about.html"), name="about"),
+    path("about/annual-reports/", template("web/about/reports.html"), name="reports"),
+    path("about/team/", views.team_view, name="team"),
+    path("about/impact/", template("web/about/impact.html"), name="impact"),
+    path(
+        "services/rental-repairs/",
+        template("web/services/repairs.html"),
+        name="repairs",
+    ),
+    path(
+        "services/eviction-support/",
+        template("web/services/evictions.html"),
+        name="evictions",
+    ),
+    path("/blog/", template("web/blog/blog-list.html"), name="blog"),
     path("robots.txt", views.robots_view),
     path("landing/contact/", views.landing_contact_form_view, name="landing-contact"),
     path("", views.landing_view, name="landing"),
--- app/web/views.py
+++ app/web/views.py
@@ -16,7 +16,9 @@ def robots_view(request):
 @require_http_methods(["GET"])
 def landing_view(request):
     form = ContactForm()
-    return render(request, "web/landing.html", {"form": form})
+    return render(
+        request, "web/landing.html", {"form": form, "testimonials": TESTIMONIALS}
+    )
 
 
 @require_http_methods(["POST"])
@@ -33,7 +35,9 @@ def landing_contact_form_view(request):
 def team_view(request):
     members = [TEAM_MEMBERS[0]] + shuffle(TEAM_MEMBERS[1:])
     advisors = shuffle(ADVISORS)
-    return render(request, "web/team.html", {"members": members, "advisors": advisors})
+    return render(
+        request, "web/about/team.html", {"members": members, "advisors": advisors}
+    )
 
 
 def shuffle(l):
@@ -154,5 +158,33 @@ TEAM_MEMBERS = [
             "Matt leads our Technology portfolio and is responsible for our technical infrastructure",
             "Matt is a full-stack software developer with experience in eCommerce, fintech and online media",
         ],
+    },
+]
+
+TESTIMONIALS = [
+    {
+        "name": "Gabrielle",
+        "testimonial": "After months of getting nowhere with my landlord I was beginning to think I was overreacting to a number of faults in my house, namely heating that didn't work. Contacting Anika to get some advice and legal support was super easy and super fast. I was able to have a chat on the phone to a staff member, voice my concerns and ask questions, which was followed up by timely legal advice. Suffice to say, my heating ended up getting fixed the following week without going to VCAT, and I was able to maintain a relationship with my agent and landlord which was something I was anxious about. It is amazing to have free support for tenants who have very little idea about their rights. Anika has filled a very large gap in the system and I couldn't recommend them enough. Thanks again to the team!",
+        "image": "web/img/testimonials/gabrielle.jpg",
+    },
+    {
+        "name": "Hieu",
+        "testimonial": "[Anika] was really quick with everything, like the follow-up, and also tried their best to help out. I feel like I at least had someone else on my side who helped me work things out.. If it wasn’t for Anika I don’t think I would have pursued the negotiations on my own. Now [my rent] is actually reduced.",
+        "image": "web/img/testimonials/hieu.jpg",
+    },
+    {
+        "name": "Erica",
+        "testimonial": "I would give Anika a 10! I didn’t have any issues at all and I thought the service went super well. It was exactly what I needed. Having the law on your side makes you feel way more comfortable with dealing with these things, especially because in the past I have been told that I am just a young renter who doesn’t have any rights. I didn’t realise that as a renter I actually had some power!",
+        "image": "web/img/testimonials/erica.jpg",
+    },
+    {
+        "name": "Louise",
+        "testimonial": "Anika was exactly the service I needed - I couldn’t have asked for anything better. I was so used to everyone else that I’d dealt with letting me down - agents, landlords, even law firms. Sam (the Anika law student) was just that one solid person I could completely count on when I needed him. The empathy that everyone at Anika showed for our situation was amazing, and with Sam taking over, everything just became so much easier for me.",
+        "image": "web/img/testimonials/louise.jpg",
+    },
+    {
+        "name": "Mary",
+        "testimonial": "Big thank you to Anika Legal and the team. Without their legal advice I would not have been able to get my repairs done. I am very thankful, the communication and help received was incredible.",
+        "image": "web/img/testimonials/mary.jpg",
     },
 ]
