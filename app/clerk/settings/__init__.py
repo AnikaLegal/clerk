fd3fc23aaf6267e210ac398d03208cd7e928186a
Matthew Segal <mattdsegal@gmail.com>
2021-05-21 16:48:11 +1000

    Extend login whitelist

--- app/clerk/settings/__init__.py
+++ app/clerk/settings/__init__.py
@@ -121,8 +121,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
     "https://www.googleapis.com/auth/userinfo.profile",
 ]
 
-# SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_DOMAINS = ["anikalegal.com"]
-SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_EMAILS = ["matt@anikalegal.com"]
+SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_DOMAINS = ["anikalegal.com"]
 
 # See https://python-social-auth.readthedocs.io/en/latest/configuration/django.html
 SOCIAL_AUTH_PIPELINE = (
