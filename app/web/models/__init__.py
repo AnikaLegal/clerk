6650cf80f54d141e435b1e2c5054e7571d3f1efe
Matthew Segal <mattdsegal@gmail.com>
2021-06-30 16:00:42 +1000

    Fix import for migrations

--- app/web/models/__init__.py
+++ app/web/models/__init__.py
@@ -1,5 +1,5 @@
 from .blog import BlogListPage, BlogPage
-from .jobs import JobListPage, JobPage
+from .jobs import JobListPage, JobPage, JobsRootMixin
 from .resources import ResourceListPage, ResourcePage
 from .root import RootPage
 from .web_redirect import WebRedirect
