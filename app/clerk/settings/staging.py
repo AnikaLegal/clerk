dc1d96aad6bb5fa9b0eb6c5fa2540c54615ca170
Matthew Segal <mattdsegal@gmail.com>
2021-08-28 13:01:25 +1000

    Allow staging to create prod Actionstep tokens

--- app/clerk/settings/staging.py
+++ app/clerk/settings/staging.py
@@ -53,6 +53,15 @@ MAILCHIMP_REPAIRS_EMAIL_ID = "04fb17ccee"
 
 # Call Centre powered by Twilio
 TWILIO_PHONE_NUMBER = "+61480015687"
-TWILIO_AUDIO_BASE_URL = "https://anika-twilio-audio-test.s3-ap-southeast-2.amazonaws.com"
+TWILIO_AUDIO_BASE_URL = (
+    "https://anika-twilio-audio-test.s3-ap-southeast-2.amazonaws.com"
+)
 
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+
+ACTIONSTEP_SYNC = False  # Only sync in prod and test, never dev or staging
+ACTIONSTEP_REDIRECT_URI = "https://test-clerk.anikalegal.com/actionstep/end/"
+ACTIONSTEP_WEB_URI = "https://ap-southeast-2.actionstep.com"
+ACTIONSTEP_OAUTH_URI = "https://go.actionstep.com"
+ACTIONSTEP_TOKEN_URI = "https://api.actionstep.com"
+ACTIONSTEP_SETUP_OWNER = "coordinators@anikalegal.com"
