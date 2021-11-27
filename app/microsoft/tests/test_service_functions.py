5154a3ca1467f736361193e84804ba9a97c6565f
Peter Zhang <lpzhang@me.com>
2021-11-27 15:10:32 +1100

    Write tests for set_up_new_user service function

--- app/microsoft/tests/test_service_functions.py
+++ app/microsoft/tests/test_service_functions.py
@@ -9,7 +9,7 @@ from core.factories import UserFactory
 
 @pytest.fixture
 def mock_client():
-    """DRY: stub helper methods called by the service function for each test case."""
+    """Stub helper methods called when MSGraphAPI object is created."""
     with mock.patch("microsoft.endpoints.create_client") as mock_create_client:
         mock_client = mock.Mock()
         mock_client.acquire_token_silent.return_value = {"access_token": "1812"}
@@ -17,11 +17,52 @@ def mock_client():
         yield mock_client
 
 
+@pytest.fixture
+def mock_api():
+    """Mock MSGraph API object instantiated by each service function."""
+    with mock.patch("microsoft.service.MSGraphAPI") as mock_MSGraphAPI:
+        mock_api = mock.Mock()
+        mock_MSGraphAPI.return_value = mock_api
+        yield mock_api
+
+
 def test_MSGraphAPI(mock_client):
-    """Check MSGraphAPI constructor obtains access token and uses it to instantiate Endpoint objects."""
+    """MSGraphAPI contructor obtains access token and uses it to instantiate Endpoint objects."""
     api = MSGraphAPI()
 
     mock_client.acquire_token_silent.assert_called_once()
     assert api.group.headers["Authorization"] == "Bearer 1812"
     assert api.user.headers["Authorization"] == "Bearer 1812"
     assert api.folder.headers["Authorization"] == "Bearer 1812"
+
+
+@pytest.mark.django_db
+def test_set_up_new_user_A(mock_api):
+    """Check set_up_new_user service function works correctly for existing user."""
+    user = UserFactory()
+    mock_api.user.get.return_value = {
+        f"Microsoft Account already exists for {user.email}"
+    }
+
+    set_up_new_user(user)
+
+    mock_api.user.get.assert_called_once_with(user.email)
+    mock_api.user.create.assert_not_called()
+    mock_api.user.assign_license.assert_not_called()
+
+
+@pytest.mark.django_db
+def test_set_up_new_user_B(mock_api):
+    """Check set_up_new_user service function works correctly for new user."""
+    user = UserFactory()
+    mock_api.user.get.return_value = None
+    mock_api.user.create.return_value = user, "open sesame"
+
+    password = set_up_new_user(user)
+
+    mock_api.user.get.assert_called_once_with(user.email)
+    mock_api.user.create.assert_called_once_with(
+        user.first_name, user.last_name, user.email
+    )
+    mock_api.user.assign_license.assert_called_once_with(user.email)
+    assert password == "open sesame"
