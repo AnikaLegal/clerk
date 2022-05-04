857becf7884efd2db2be503e7eeee6b87bc5a90c
Matthew Segal <mattdsegal@gmail.com>
2022-05-04 12:59:12 +1000

    Remove UFW

--- infra/security/setup.sh
+++ infra/security/setup.sh
@@ -10,14 +10,6 @@ apt-get upgrade --yes
 sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
 systemctl restart sshd
 
-# Enable firewall
-ufw default allow outgoing
-ufw default deny incoming
-ufw allow 22
-ufw allow 80
-ufw enable
-ufw status
-
 # TODO: Enable unattended upgrades (not sure best way to do)
 # apt-get install unattended-upgrades
 # dpkg-reconfigure -f noninteractive unattended-upgrades
