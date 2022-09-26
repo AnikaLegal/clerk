34d69b6c22a328c671e88833a28f69c2181899a8
Matthew Segal <mattdsegal@gmail.com>
2022-09-26 13:15:01 +1000

    Start notify app

---
+++ app/notify/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from core.models.timestamped import TimestampedModel
+
+
+class NotifyEvent(models.TextChoices):
+    """
+    Events that can trigger a notification.
+
+    Future considerations may be:
+    - case first created
+    - case open/closed change
+    - case assignment change
+    - case outcome change
+    - email received
+    """
+
+    STAGE_CHANGE = "STAGE_CHANGE", "Stage changed"
+
+
+class NotifyChannel(models.TextChoices):
+    """
+    Channels which can be used to notify a user.
+
+    Future considerations may be:
+    - email
+    - in-app notification system
+    """
+
+    SLACK = "SLACK", "Send a Slack message"
+
+
+class NotifyTarget(models.TextChoices):
+    """
+    Who gets notified about the event.
+    """
+
+    PARALEGAL = "PARALEGAL", "Assigned paralegal"
+    LAWYER = "LAWYER", "Assigned lawyer"
+
+
+class Notification(TimestampedModel):
+    """
+    A notification that is sent,
+    triggered by an Issue event,
+    sending text to a target user via a channel
+    """
+
+    event = models.CharField(max_length=32, choices=NotifyEvent.CHOICES)
+    channel = models.CharField(max_length=32, choices=NotifyChannel.CHOICES)
+    target = models.CharField(max_length=32, choices=NotifyTarget.CHOICES)
+    text = models.TextField()
