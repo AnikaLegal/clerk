5c5d539ed183789c418ea35c462e11cb84930b70
Luca Vari <luca.vari@gmail.com>
2023-12-07 16:11:18 +1100

    Move formatting code from model to utility class

--- app/office/models.py
+++ app/office/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.apps import apps
 from django.utils import timezone, dateformat
-
 from pathlib import Path
 
 PATH = apps.get_app_config("office").path
-DATE_FORMAT = "l jS F Y"  # e.g. Sunday 31st December 2023
 
 
 class ShutdownTemplate(models.Model):
@@ -39,9 +37,3 @@ class Shutdown(models.Model):
         default=_get_template_default,
         on_delete=models.CASCADE,
     )
-
-    @property
-    def notice_html(self):
-        start = dateformat.format(self.start_date, DATE_FORMAT)
-        end = dateformat.format(self.end_date, DATE_FORMAT)
-        return self.template.notice_html.format(start_date=start, end_date=end)
--- app/office/shutdown/service.py
+++ app/office/shutdown/service.py
@@ -1,11 +1,13 @@
 from office.models import Shutdown
+from office.shutdown.shutdown_handler import ShutdownHandler
 from django.utils import timezone
 
 
 def get_office_shutdown():
     date = timezone.localdate()
-    return (
+    shutdown = (
         Shutdown.objects.filter(start_date__lte=date, end_date__gte=date)
         .order_by("created_at")
         .last()
     )
+    return ShutdownHandler(shutdown)
