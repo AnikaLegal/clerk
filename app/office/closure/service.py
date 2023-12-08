486099234ae14920d4a1ebbff166677341a3ec2a
Luca Vari <luca.vari@gmail.com>
2023-12-08 14:35:30 +1100

    Add support for office closures

--- app/office/closure/service.py
+++ app/office/closure/service.py
@@ -5,13 +5,13 @@ import datetime
 DATE_FORMAT = "l jS F Y"  # e.g. Sunday 31st December 2023
 
 
-def _format(start_date: datetime.date, end_date: datetime.date, value: str):
+def _format(start_date: datetime.date, end_date: datetime.date, value: str) -> str:
     start = dateformat.format(start_date, DATE_FORMAT)
     end = dateformat.format(end_date, DATE_FORMAT)
     return value.format(start_date=start, end_date=end)
 
 
-def get_closure():
+def get_closure() -> Closure | None:
     date = timezone.localdate()
     return (
         Closure.objects.filter(start_date__lte=date, end_date__gte=date)
@@ -20,7 +20,16 @@ def get_closure():
     )
 
 
-def get_closure_notice():
+def get_closure_email() -> str | None:
+    closure = get_closure()
+    if closure:
+        return _format(
+            closure.start_date, closure.end_date, closure.template.email_html
+        )
+    return None
+
+
+def get_closure_notice() -> str | None:
     closure = get_closure()
     if closure:
         return _format(
