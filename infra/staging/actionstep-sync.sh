4fb73ef1458216682729f3d600a0e30299a2f893
Luca Vari <luca.vari@gmail.com>
2024-02-13 14:31:59 +1100

    Use integrated docker compose command

--- infra/staging/actionstep-sync.sh
+++ infra/staging/actionstep-sync.sh
@@ -4,7 +4,7 @@ HOST='13.55.250.149'
 PROJECT='staging'
 
 echo -e "\n>>> Copying staging compose file to clerk at $HOST"
-scp -o StrictHostKeyChecking=no  docker/docker-compose.staging.yml root@${HOST}:/srv/clerk_test/
+scp -o StrictHostKeyChecking=no docker/docker-compose.staging.yml root@${HOST}:/srv/clerk_test/
 
 
 echo -e "\n>>> SSHing into clerk at $HOST."
@@ -12,7 +12,7 @@ ssh -o StrictHostKeyChecking=no root@$HOST /bin/bash << EOF
     set -e
     cd /srv/clerk_test/
     docker pull anikalaw/clerk:staging
-    docker-compose \
+    docker compose \
         -p task \
         -f docker-compose.staging.yml \
         run --rm web \
