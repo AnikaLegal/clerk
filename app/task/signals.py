849c087389f2d422f6b9dccc1802f584212177a2
Luca Vari <luca.vari@gmail.com>
2024-05-06 14:22:14 +1000

    Refactor import

--- app/task/signals.py
+++ app/task/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_q.tasks import async_task
 
-from core.models.issue_event import IssueEvent
+from core.models import IssueEvent
 from .tasks import maybe_create_tasks
 
 
