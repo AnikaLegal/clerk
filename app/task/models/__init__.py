7aac08b9ecf3b5ce0a94063e754f646c5ad8193a
Luca Vari <luca.vari@gmail.com>
2024-05-14 13:46:00 +1000

    Add types to distinguish system & user comments

--- app/task/models/__init__.py
+++ app/task/models/__init__.py
@@ -1,3 +1,4 @@
 from .task import Task
 from .template import TaskTemplate
 from .trigger import TaskTrigger
+from .comment import TaskComment
--- app/task/models/comment.py
+++ app/task/models/comment.py
@@ -2,19 +2,23 @@ from django.db import models
 
 from accounts.models import User
 from core.models import TimestampedModel
-from .task import Task
 
 
 class CommentType(models.TextChoices):
-    # TODO
-    pass
+    SYSTEM = "SYSTEM", "System generated"
+    USER = "USER", "User created"
 
 
 class TaskComment(TimestampedModel):
     """
     A comment made on a task.
     """
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    task = models.ForeignKey("task.Task", on_delete=models.CASCADE)
     type = models.CharField(max_length=32, choices=CommentType.choices)
     creator = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
     text = models.TextField(blank=True, default="")
+
+    class Meta:
+        verbose_name = "comment"
+
