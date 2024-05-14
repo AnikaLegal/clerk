6506006fd2f76779a14885c40297147050c97d5d
Luca Vari <luca.vari@gmail.com>
2024-05-14 13:49:05 +1000

    Add task comments admin interface & change some task admin fields to links

--- app/task/admin.py
+++ app/task/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.forms import Textarea
 from django.db import models
-from task.models import Task, TaskTrigger, TaskTemplate
+
+from task.models import Task, TaskTrigger, TaskTemplate, TaskComment
+from utils.admin import admin_link
 
 
 class TemplateInline(admin.TabularInline):
@@ -49,8 +51,35 @@ class TaskAdmin(admin.ModelAdmin):
         "id",
         "type",
         "name",
-        "description",
+        "owner_link",
+        "assigned_to_link",
         "status",
         "is_open",
     )
     readonly_fields = ("is_open",)
+
+    @admin_link("owner", "Owner")
+    def owner_link(self, user):
+        return user if user else None
+
+    @admin_link("assigned_to", "Assigned To")
+    def assigned_to_link(self, user):
+        return user if user else None
+
+
+@admin.register(TaskComment)
+class TaskCommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "type",
+        "task_link",
+        "creator_link",
+    )
+
+    @admin_link("task", "Task")
+    def task_link(self, task):
+        return task if task else None
+
+    @admin_link("creator", "Creator")
+    def creator_link(self, user):
+        return user if user else None
