41e36f02a550d1341ccc610179f92fd467a44080
Luca Vari <luca.vari@gmail.com>
2024-05-23 14:05:57 +1000

    Add task serializer

---
+++ app/task/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from .models import Task
+from .models.template import TaskType
+from .models.task import TaskStatus
+from case.serializers.fields import TextChoiceField
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = (
+            "id",
+            "type",
+            "name",
+            "description",
+            "status",
+            "is_open",
+            "is_suspended",
+            "issue_id",
+            "owner_id",
+            "assigned_to_id",
+        )
+
+    type = TextChoiceField(TaskType)
+    status = TextChoiceField(TaskStatus)
+    issue_id = serializers.UUIDField()
+    owner_id = serializers.IntegerField()
+    assigned_to_id = serializers.IntegerField()
