21bee33e125a64b6321f0becb4d2b3cd23493cd1
Luca Vari <luca.vari@gmail.com>
2024-05-27 14:58:15 +1000

    Prevent occasional DataError when running tests
    The full error is
    
      django.db.utils.DataError: value too long for type character varying(64)
    
    Occasionally the faker sentence provider generates text with more than 64
    characters which is that maximum length of the Task name field. You cannot
    specify a maximum length with faker so we try for text around but not over the
    max length.

--- app/task/factories.py
+++ app/task/factories.py
@@ -37,7 +37,7 @@ class TaskFactory(TimestampedModelFactory):
         model = Task
 
     type = factory.Faker("random_element", elements=[c[0] for c in TaskType.choices])
-    name = factory.Faker("sentence")
+    name = factory.Faker("text", max_nb_chars=45)
     description = factory.Faker("paragraph")
     status = TaskStatus.NOT_STARTED
     issue = factory.SubFactory(IssueFactory)
