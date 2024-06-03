c4a334a17f337c641f66b4e990ec4fbf2573cfa1
Luca Vari <luca.vari@gmail.com>
2024-06-03 13:31:29 +1000

    Set username the same as obfuscated email
    Otherwise the previous, non-obfuscated name remains in the username field.

--- app/core/management/commands/obfuscate_data.py
+++ app/core/management/commands/obfuscate_data.py
@@ -45,6 +45,7 @@ class Command(BaseCommand):
 
         for u in users:
             u.email = fake.email()
+            u.username = u.email # Username same as fake email.
             u.first_name = fake.first_name()
             u.last_name = fake.last_name()
             u.save()
