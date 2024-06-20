032af52d2a55f8d0eb8e1e0a324617a28d154e2d
Luca Vari <luca.vari@gmail.com>
2024-06-20 10:59:03 +1000

    Use TableForm component for UI

--- frontend/src/pages/task-detail.tsx
+++ frontend/src/pages/task-detail.tsx
@@ -1,11 +1,12 @@
-import React from "react"
-import { Formik } from "formik"
+import React, { useState, useEffect } from "react"
 import { Container, Header } from "semantic-ui-react"
-import { useSnackbar } from "notistack"
+import * as Yup from "yup"
 
-import { mount, getAPIErrorMessage, getAPIFormErrors } from "utils"
-import { useGetTaskQuery, useDeleteTaskMutation, useUpdateTaskMutation, Task } from "api"
-import { choiceToMap } from "utils"
+import { TableForm } from "comps/table-form"
+import { getFormSchema, FormField, Model, Choices } from 'comps/auto-form'
+import { FIELD_TYPES } from "comps/field-component"
+import { mount } from "utils"
+import api, { Task, TaskCreate } from "api"
 
 interface DjangoContext {
   choices: {
@@ -15,122 +16,78 @@ interface DjangoContext {
   task_pk: string
   list_url: string
 }
-
 const CONTEXT = (window as any).REACT_CONTEXT as DjangoContext
-const STATUS_LABELS = choiceToMap(CONTEXT.choices.status)
-const TYPE_LABELS = choiceToMap(CONTEXT.choices.type)
 
 const App = () => {
-  const [updateTask, updateTaskResult] = useUpdateTaskMutation()
-  const [deleteTask] = useDeleteTaskMutation()
-  const { enqueueSnackbar } = useSnackbar()
+  const [isLoading, setIsLoading] = useState(true)
+  const [task, setTask] = useState<Task>()
+  const [getTask] = api.useLazyGetTaskQuery()
+  const [updateTask] = api.useUpdateTaskMutation()
 
-  const taskResult = useGetTaskQuery({ id: CONTEXT.task_pk })
-  const isInitialLoad = taskResult.isLoading
-  if (isInitialLoad) return null
-  const task = taskResult.data
+  useEffect(() => {
+    setIsLoading(true)
+    getTask({ id: CONTEXT.task_pk })
+      .unwrap()
+      .then((task) => {
+        setTask(task)
+        setIsLoading(false)
+      })
+      .catch(() => setIsLoading(false))
+  }, [])
+  if (isLoading) return null
 
-  const handleDelete = (e) => {
-    e.preventDefault()
-    if (window.confirm("Are you sure you want to delete this task?")) {
-      deleteTask({ id: task.id })
-        .then(() => {
-          window.location.href = CONTEXT.list_url
-        })
-        .catch((err) => {
-          enqueueSnackbar(getAPIErrorMessage(err, "Failed to delete task"), {
-            variant: "error",
-          })
-        })
-    }
-  }
+  const update = (id: string, values: { [fieldName: string]: unknown }) =>
+    updateTask({
+      id: task.id,
+      taskCreate: { ...values, "issue_id": task.issue_id } as TaskCreate,
+    }).unwrap()
+
+  const choices = (formFields: FormField[], model: Model) =>
+    formFields.reduce<Choices>((accumulator, field) => {
+      const choices = CONTEXT.choices?.[field.name]
+      if (choices) {
+        return { ...accumulator, [field.name]: choices }
+      } else {
+        return accumulator
+      }
+    }, {})
+
   return (
     <Container>
-      <TaskHeader task={task} />
-      <Formik
-        initialValues={{
-          name: task.name,
-          description: task.description,
-        }}
-        validate={(values) => { }}
-        onSubmit={(values, { setSubmitting, setErrors }) => {
-          updateTask({ id: task.id, taskCreate: values })
-            .unwrap()
-            .then((task) => {
-              enqueueSnackbar("Updated task", { variant: "success" })
-              setSubmitting(false)
-            })
-            .catch((err) => {
-              enqueueSnackbar(
-                getAPIErrorMessage(err, "Failed to update task"), {
-                variant: "error",
-              })
-              const requestErrors = getAPIFormErrors(err)
-              if (requestErrors) {
-                setErrors(requestErrors)
-              }
-              setSubmitting(false)
-            })
-        }}
-      >
-      </Formik>
+      <Header as="h1">{task.name}</Header>
+      <Header as="h3">Task details</Header>
+      <TableForm
+        fields={FIELDS}
+        schema={SCHEMA}
+        model={task}
+        setModel={setTask}
+        modelName="task"
+        onUpdate={update}
+        choices={choices}
+      />
     </Container>
   )
 }
 
-interface TaskHeaderProps {
-  task: Task
-}
-export const TaskHeader: React.FC<TaskHeaderProps> = ({
-  task,
-}) => {
-  return (
-    <>
-      <Header as="h1">
-        {task.name} (<a href={task.issue.url}>{task.issue.fileref}</a>)
-        <div className="sub header">
-          <div>
-            Created {task.created_at}{task.closed_at && (", Closed ") + task.closed_at}
-          </div>
-          <div>
-            {task.assigned_to ? (
-              <span>
-                Assigned to&nbsp;
-                <a href={task.assigned_to.url}>{task.assigned_to.full_name},</a>
-                &nbsp;
-              </span>
-            ) : (
-              "Not assigned, "
-            )}
-            {task.owner ? (
-              <span>
-                Owned by&nbsp;
-                <a href={task.owner.url}>{task.owner.full_name}</a>&nbsp;
-              </span>
-            ) : (
-              "not owned"
-            )}
-          </div>
-        </div>
-      </Header>
-      <span id="case-status" hx-swap-oob="true">
-        {task.is_open ? (
-          <div className="ui blue label">Task open</div>
-        ) : (
-          <div className="ui green label">Task closed</div>
-        )}
-
-        <div className="ui grey label">
-          Status
-          {task.status ? (
-            <div className="detail">{STATUS_LABELS.get(task.status)}</div>
-          ) : (
-            <div className="detail">-</div>
-          )}
-        </div>
-      </span>
-    </>
-  )
-}
+const FIELDS = [
+  {
+    label: "Name",
+    schema: Yup.string().required("Required"),
+    type: FIELD_TYPES.TEXT,
+    name: "name",
+  },
+  {
+    label: "Type",
+    schema: Yup.string().required("Required"),
+    type: FIELD_TYPES.SINGLE_CHOICE,
+    name: "type",
+  },
+  {
+    label: "Description",
+    type: FIELD_TYPES.TEXTAREA,
+    name: "description",
+  },
+]
+const SCHEMA = getFormSchema(FIELDS)
 
 mount(App)
