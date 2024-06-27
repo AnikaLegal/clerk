314a8ef0607a5b96130f0d1d95a13bfbc44ef40a
Luca Vari <luca.vari@gmail.com>
2024-06-27 12:19:16 +1000

    Support updating status & add some dates to rail
    Also attempt some minimal code tidy.

--- frontend/src/pages/task-detail.tsx
+++ frontend/src/pages/task-detail.tsx
@@ -1,53 +1,39 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useMemo } from "react"
+import { Container, Header, Grid, Divider, Segment, Rail, Form, Button, Dropdown } from "semantic-ui-react"
 
-import { Container, Header, Grid, Divider } from "semantic-ui-react"
-import { Segment, Rail, Form, Button, Dropdown } from "semantic-ui-react"
+import api, { Task, TaskCreate } from "api"
+import { getAPIErrorMessage, getAPIFormErrors, mount, choiceToMap, choiceToOptions, markdownToHtml } from 'utils'
+
+import { ModelId, ModelType, Model, SetModel, UpdateModel, ModelChoices } from "types"
+import { getFormSchema, AutoForm, getModelInitialValues, } from 'comps/auto-form'
+import { FIELD_TYPES } from "comps/field-component"
+
+import { Formik } from 'formik'
 import { useSnackbar } from 'notistack'
 import * as Yup from "yup"
 
-import { getFormSchema, FormField } from 'comps/auto-form'
-import { FIELD_TYPES } from "comps/field-component"
-import { mount, choiceToMap, choiceToOptions } from "utils"
-import api, { Task, TaskCreate } from "api"
-import { markdownToHtml } from 'utils'
-
-import { Formik } from 'formik'
-import { getAPIErrorMessage, getAPIFormErrors } from 'utils'
-import {
-  AutoForm,
-  getModelInitialValues,
-} from 'comps/auto-form'
-
-import { ModelId, ModelType, Model, SetModel, ModelChoices } from "types"
-
 interface DjangoContext {
   choices: {
-    status: string[][]
-    type: string[][]
+    status: [string, string][]
+    type: [string, string][]
   }
   task_pk: string
   list_url: string
 }
 const CONTEXT = (window as any).REACT_CONTEXT as DjangoContext
-const TYPE_LABELS = choiceToMap(CONTEXT.choices.type)
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [task, setTask] = useState<Task>()
-  const [getTask] = api.useLazyGetTaskQuery()
-  const [updateTask] = api.useUpdateTaskMutation()
+  const taskResult = api.useGetTaskQuery({ id: CONTEXT.task_pk })
+  if (taskResult.isLoading)
+    return null
+  return (
+    <TaskDetails data={taskResult.data} choices={CONTEXT.choices} />
+  )
+}
 
-  useEffect(() => {
-    setIsLoading(true)
-    getTask({ id: CONTEXT.task_pk })
-      .unwrap()
-      .then((task) => {
-        setTask(task)
-        setIsLoading(false)
-      })
-      .catch(() => setIsLoading(false))
-  }, [])
-  if (isLoading) return null
+export const TaskDetails = ({ data, choices }: { data: Task, choices: ModelChoices }) => {
+  const [task, setTask] = useState<Task>(data)
+  const [updateTask] = api.useUpdateTaskMutation()
 
   const update = (id: ModelId, values: Model) =>
     updateTask({
@@ -55,71 +41,39 @@ const App = () => {
       taskCreate: values as TaskCreate,
     }).unwrap()
 
-  const choices = (formFields: FormField[], model: Model) =>
-    formFields.reduce<ModelChoices>((accumulator, field) => {
-      const choices = CONTEXT.choices?.[field.name]
-      if (choices) {
-        return { ...accumulator, [field.name]: choices }
-      } else {
-        return accumulator
-      }
-    }, {})
-
   return (
     <Container>
       <Segment basic>
         <TaskBody task={task} setTask={setTask} update={update} choices={choices} />
-        <Rail dividing position="right">
-          <Segment basic>
-            <Grid>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header sub>Status</Header>
-                  <Dropdown
-                    value={task.status}
-                    options={choiceToOptions(CONTEXT.choices.status)}
-                  />
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row columns={2}>
-                <Grid.Column>
-                  <Header sub>Fileref</Header>
-                  <a href={task.issue.url}>{task.issue.fileref}</a>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header sub>Days Open</Header>
-                  {task.days_open}
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row columns={2}>
-                <Grid.Column>
-                  <Header sub>Assigned To</Header>
-                  <a href={task.assigned_to.url}>{task.assigned_to.full_name}</a>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header sub>Owner</Header>
-                  <a href={task.owner.url}>{task.owner.full_name}</a>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment>
-        </Rail>
+        <TaskRail task={task} setTask={setTask} update={update} choices={choices} />
         <Divider />
+        {/* 
+          <TaskComments />
+        */}
       </Segment>
     </Container>
   )
 }
 
-interface TaskBodyProps<Type extends ModelType> {
-  task: Type,
-  setTask: SetModel<Type>,
-  update: (id: ModelId, values: Model) => Promise<Type>
-  choices: (formFields: FormField[], model: Type) => ModelChoices
+export const MarkdownDisplay = ({ value }: { value: string }) => {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: value ? markdownToHtml(value) : '-' }} />
+  )
 }
 
-export const TaskBody = ({ task, setTask, update, choices }: TaskBodyProps<Task>) => {
-  const { enqueueSnackbar } = useSnackbar()
+
+interface TaskProps<Type extends ModelType> {
+  task: Type,
+  setTask: SetModel<Type>,
+  update: UpdateModel<Type>,
+  choices: ModelChoices
+}
+
+export const TaskBody = ({ task, setTask, update, choices }: TaskProps<Task>) => {
   const [isEditMode, setEditMode] = useState(false)
+  const typeLabels = useMemo(() => choiceToMap(choices.type), [])
+
+  const { enqueueSnackbar } = useSnackbar()
   const toggleEditMode = () => setEditMode(!isEditMode)
 
   if (!isEditMode) {
@@ -130,7 +84,7 @@ export const TaskBody = ({ task, setTask, update, choices }: TaskBodyProps<Task>
             <Header as="h1">
               {task.name}
               <Header.Subheader>
-                {TYPE_LABELS.get(task.type)}
+                {typeLabels.get(task.type)}
               </Header.Subheader>
             </Header>
           </Grid.Column>
@@ -144,7 +98,7 @@ export const TaskBody = ({ task, setTask, update, choices }: TaskBodyProps<Task>
           <Grid.Column>
             <Form>
               <Form.Field>
-                <div dangerouslySetInnerHTML={{ __html: task.description ? markdownToHtml(task.description) : '-' }} />
+                <MarkdownDisplay value={task.description} />
               </Form.Field>
             </Form>
           </Grid.Column>
@@ -153,40 +107,57 @@ export const TaskBody = ({ task, setTask, update, choices }: TaskBodyProps<Task>
     )
   }
 
+  const submit = (values: Model, { setSubmitting, setErrors }: any) => {
+    update(task.id, values)
+      .then((instance) => {
+        enqueueSnackbar(`Updated task`, { variant: 'success' })
+        setTask(instance)
+        toggleEditMode()
+        setSubmitting(false)
+      })
+      .catch((err) => {
+        enqueueSnackbar(
+          getAPIErrorMessage(err, `Failed to update this task`),
+          {
+            variant: 'error',
+          }
+        )
+        const requestErrors = getAPIFormErrors(err)
+        if (requestErrors) {
+          setErrors(requestErrors)
+        }
+        setSubmitting(false)
+      })
+  }
+
+  const fields = [
+    {
+      label: "Name",
+      schema: Yup.string().required("Required"),
+      type: FIELD_TYPES.TEXT,
+      name: "name",
+    },
+    {
+      label: "Type",
+      schema: Yup.string().required("Required"),
+      type: FIELD_TYPES.SINGLE_CHOICE,
+      name: "type",
+    },
+    {
+      label: "Description",
+      type: FIELD_TYPES.TEXTAREA,
+      name: "description",
+    },
+  ]
+  const schema = getFormSchema(fields)
+
   return (
-    <Formik
-      initialValues={getModelInitialValues(FIELDS, task)}
-      validationSchema={SCHEMA}
-      onSubmit={(
-        values: { [fieldName: string]: unknown },
-        { setSubmitting, setErrors }: any
-      ) => {
-        update(task.id, values)
-          .then((instance) => {
-            enqueueSnackbar(`Updated task`, { variant: 'success' })
-            setTask(instance)
-            toggleEditMode()
-            setSubmitting(false)
-          })
-          .catch((err) => {
-            enqueueSnackbar(
-              getAPIErrorMessage(err, `Failed to update this task`),
-              {
-                variant: 'error',
-              }
-            )
-            const requestErrors = getAPIFormErrors(err)
-            if (requestErrors) {
-              setErrors(requestErrors)
-            }
-            setSubmitting(false)
-          })
-      }}
-    >
+    <Formik initialValues={getModelInitialValues(fields, task)}
+      validationSchema={schema} onSubmit={submit} >
       {(formik) => (
         <AutoForm
-          fields={FIELDS}
-          choices={choices(FIELDS, task)}
+          fields={fields}
+          choices={choices}
           formik={formik}
           onCancel={toggleEditMode}
           submitText="Update"
@@ -196,26 +167,74 @@ export const TaskBody = ({ task, setTask, update, choices }: TaskBodyProps<Task>
   )
 }
 
+export const TaskRail = ({ task, setTask, update, choices }: TaskProps<Task>) => {
+  const statusOptions = useMemo(() => choiceToOptions(choices.status), [])
+  const { enqueueSnackbar } = useSnackbar()
 
-const FIELDS = [
-  {
-    label: "Name",
-    schema: Yup.string().required("Required"),
-    type: FIELD_TYPES.TEXT,
-    name: "name",
-  },
-  {
-    label: "Type",
-    schema: Yup.string().required("Required"),
-    type: FIELD_TYPES.SINGLE_CHOICE,
-    name: "type",
-  },
-  {
-    label: "Description",
-    type: FIELD_TYPES.TEXTAREA,
-    name: "description",
-  },
-]
-const SCHEMA = getFormSchema(FIELDS)
+  const handleChange = (name: string, value: string) => {
+    update(task.id, { [name]: value })
+      .then((instance) => {
+        enqueueSnackbar(`Updated task`, { variant: 'success' })
+        setTask(instance)
+      })
+      .catch((err) => {
+        enqueueSnackbar(
+          getAPIErrorMessage(err, `Failed to update this task`),
+          {
+            variant: 'error',
+          }
+        )
+      })
+  }
+
+  return (
+    <Rail attached dividing position="right">
+      <Segment basic>
+        <Grid>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header sub>Status</Header>
+              <Dropdown
+                value={task.status}
+                options={statusOptions}
+                onChange={(e, { value }) => handleChange("status", value)}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Header sub>Days Open</Header>
+              {task.days_open}
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              <Header sub>Fileref</Header>
+              <a href={task.issue.url}>{task.issue.fileref}</a>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header sub>Date Created</Header>
+              {task.created_at}
+            </Grid.Column>
+            <Grid.Column>
+              <Header sub>Date Closed</Header>
+              {task.closed_at || "-"}
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Header sub>Assigned To</Header>
+              <a href={task.assigned_to.url}>{task.assigned_to.full_name}</a>
+            </Grid.Column>
+            <Grid.Column>
+              <Header sub>Owner</Header>
+              <a href={task.owner.url}>{task.owner.full_name}</a>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </Rail>
+  )
+}
 
 mount(App)
