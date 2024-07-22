bfb4e2dfabc5418481ac28c6a7390919cf5dcb77
Luca Vari <luca.vari@gmail.com>
2024-07-22 15:25:18 +1000

    Fetch and display the task template list data

--- app/case/views/task_templates.py
+++ app/case/views/task_templates.py
@@ -3,7 +3,9 @@ from django.urls import reverse
 from rest_framework.decorators import api_view
 from rest_framework.viewsets import ModelViewSet
 
-from task.models import TaskTrigger
+from task.models.trigger import TaskTrigger, TriggerTopic, TasksCaseRole
+from core.models.issue_event import EventType
+from core.models.issue import CaseStage
 from task.serializers import TaskTriggerSerializer
 from case.utils.react import render_react_page
 
@@ -16,8 +18,15 @@ from case.views.auth import (
 @api_view(["GET"])
 @coordinator_or_better_required
 def template_task_list_page_view(request):
+
     context = {
         "create_url": reverse("template-task-create"),
+        "choices": {
+            "topic": TriggerTopic.choices,
+            "event": EventType.choices,
+            "event_stage": CaseStage.CHOICES,
+            "tasks_assignment_role": TasksCaseRole.choices,
+        },
     }
     return render_react_page(request, "Task Templates", "task-template-list", context)
 
--- frontend/src/pages/task-template-list.tsx
+++ frontend/src/pages/task-template-list.tsx
@@ -2,18 +2,30 @@ import React, { useState } from 'react'
 import { Button, Container, Header, Table } from 'semantic-ui-react'
 import { useSnackbar } from 'notistack'
 
-import { mount } from 'utils'
+import { mount, choiceToMap } from 'utils'
 import { FadeTransition } from 'comps/transitions'
+import { useGetTaskTriggersQuery } from 'api'
 
 interface DjangoContext {
   create_url: string
+  choices: {
+    topic: string[][]
+    event: string[][]
+    event_stage: string[][]
+    tasks_assignment_role: string[][]
+  }
 }
 
 const CONTEXT = (window as any).REACT_CONTEXT as DjangoContext
+const TOPIC_LABELS = choiceToMap(CONTEXT.choices.topic)
+const EVENT_LABELS = choiceToMap(CONTEXT.choices.event)
+const EVENT_STAGE_LABELS = choiceToMap(CONTEXT.choices.event_stage)
+const ROLES_LABELS = choiceToMap(CONTEXT.choices.tasks_assignment_role)
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false)
   const { enqueueSnackbar } = useSnackbar()
+  const taskTriggerResult = useGetTaskTriggersQuery()
+  const taskTriggers = taskTriggerResult.data || []
 
   return (
     <Container>
@@ -21,13 +33,37 @@ const App = () => {
       <a href={CONTEXT.create_url}>
         <Button primary>Create a new task template</Button>
       </a>
-      <FadeTransition in={!isLoading}>
-        <Table celled>
+      <FadeTransition in={!taskTriggerResult.isLoading}>
+        <Table celled style={{ marginTop: '1rem' }}>
           <Table.Header>
             <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Topic</Table.HeaderCell>
+              <Table.HeaderCell>Event</Table.HeaderCell>
+              <Table.HeaderCell>Event Stage</Table.HeaderCell>
+              <Table.HeaderCell>Assignment Role</Table.HeaderCell>
+              <Table.HeaderCell>Date Created</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
-          <Table.Body></Table.Body>
+          <Table.Body>
+            {taskTriggers.length < 1 && (
+              <Table.Row>
+                <td>No templates found</td>
+              </Table.Row>
+            )}
+            {taskTriggers.map((t) => (
+              <Table.Row key={t.id}>
+                <Table.Cell>
+                  <a href={t.url}>{t.name}</a>
+                </Table.Cell>
+                <Table.Cell>{TOPIC_LABELS.get(t.topic)}</Table.Cell>
+                <Table.Cell>{EVENT_LABELS.get(t.event)}</Table.Cell>
+                <Table.Cell>{EVENT_STAGE_LABELS.get(t.event_stage)}</Table.Cell>
+                <Table.Cell>{ROLES_LABELS.get(t.tasks_assignment_role)}</Table.Cell>
+                <Table.Cell>{t.created_at}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
         </Table>
       </FadeTransition>
     </Container>
