a4e3ac871fd3d45b0d28af6d342bed9f63d71187
Luca Vari <luca.vari@gmail.com>
2024-09-20 10:50:32 +1000

    Validate service type by category

--- app/case/serializers/service.py
+++ app/case/serializers/service.py
@@ -1,4 +1,9 @@
-from core.models import Service
+from core.models.service import (
+    DiscreteServiceType,
+    OngoingServiceType,
+    Service,
+    ServiceCategory,
+)
 from rest_framework import serializers
 
 
@@ -16,3 +21,18 @@ class ServiceSerializer(serializers.ModelSerializer):
         )
 
     issue_id = serializers.UUIDField()
+
+    def validate(self, data):
+        category = data.get("category")
+        type = data.get("type")
+        if category and type:
+            error_message = f"When category is {category}, type must be one of: "
+            if category == ServiceCategory.ONGOING and type not in OngoingServiceType:
+                raise serializers.ValidationError(
+                    {"type": error_message + ", ".join(OngoingServiceType)}
+                )
+            if category == ServiceCategory.DISCRETE and type not in DiscreteServiceType:
+                raise serializers.ValidationError(
+                    {"type": error_message + ", ".join(DiscreteServiceType)}
+                )
+        return data
