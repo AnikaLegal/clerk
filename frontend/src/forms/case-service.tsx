7b888d7634203e92f28a3161b262ca4162c49b9a
Luca Vari <luca.vari@gmail.com>
2024-10-01 14:01:37 +1000

    Refactor to support code reuse

--- frontend/src/forms/case-service.tsx
+++ frontend/src/forms/case-service.tsx
@@ -1,6 +1,6 @@
-import { Field, Formik } from 'formik'
+import { Field, Formik, FormikHelpers, useFormikContext } from 'formik'
 import { enqueueSnackbar } from 'notistack'
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Button,
   Dropdown,
@@ -14,39 +14,49 @@ import {
 import { ServiceCreate, useCreateCaseServiceMutation } from 'api'
 import DateInput from 'comps/date-input'
 import { TextArea } from 'comps/textarea'
-import { CaseDetailFormProps } from 'types'
+import { CaseDetailFormProps, CaseFormChoices } from 'types'
 import {
   choiceToOptions,
+  filterEmpty,
   getAPIErrorMessage,
   getAPIFormErrors,
-  filterEmpty,
 } from 'utils'
 
-export const ServiceForm: React.FC<CaseDetailFormProps> = ({
+export enum ServiceCategory {
+  Discrete = 'DISCRETE',
+  Ongoing = 'ONGOING',
+}
+
+export const ServiceForm = ({
   issue,
   onCancel,
   choices,
-}) => {
+}: CaseDetailFormProps) => {
   const [createService] = useCreateCaseServiceMutation()
 
-  const submitService = (values, { setSubmitting, setErrors }) => {
+  const handleSubmit = (
+    values: ServiceCreate,
+    { setSubmitting, setErrors }: FormikHelpers<ServiceCreate>
+  ) => {
     createService({ id: issue.id, serviceCreate: filterEmpty(values) })
       .unwrap()
       .then(() => {
-        setSubmitting(false)
         enqueueSnackbar('Service created', { variant: 'success' })
       })
-      .catch((err) => {
-        enqueueSnackbar(getAPIErrorMessage(err, 'Failed to create service'), {
+      .catch((e) => {
+        enqueueSnackbar(getAPIErrorMessage(e, 'Failed to create service'), {
           variant: 'error',
         })
-        const requestErrors = getAPIFormErrors(err)
+        const requestErrors = getAPIFormErrors(e)
         if (requestErrors) {
           setErrors(requestErrors)
         }
+      })
+      .finally(() => {
         setSubmitting(false)
       })
   }
+
   return (
     <Segment>
       <Header>Add a service</Header>
@@ -54,134 +64,18 @@ export const ServiceForm: React.FC<CaseDetailFormProps> = ({
         Record a unit of work to facilitate the collection of consistent and
         comparable data.
       </p>
-      <p>
-        Consistent and comparable data collection provides the foundation for a
-        strong, reliable evidence base that informs legal assistance policy and
-        supports planning and resource allocation decisions to ensure that
-        limited resources are directed to areas where services will have the
-        greatest benefit.
-      </p>
-      <Formik
-        initialValues={{}}
-        onSubmit={submitService}
-      >
-        {({
-          values,
-          errors,
-          handleSubmit,
-          isSubmitting,
-          setFieldValue,
-        }: {
-          values: ServiceCreate
-          errors: any
-          handleSubmit: any
-          isSubmitting: any
-          setFieldValue: any
-        }) => {
-          const handleChange = (e, { name, value }) =>
-            setFieldValue(name, value, false)
-
+      <Formik initialValues={{} as ServiceCreate} onSubmit={handleSubmit}>
+        {({ values, handleSubmit, isSubmitting, errors }) => {
           return (
             <Form
               onSubmit={handleSubmit}
               error={Object.keys(errors).length > 0}
             >
-              <Field
-                as={Dropdown}
-                fluid
-                selection
-                name="category"
-                loading={isSubmitting}
-                placeholder="Select the service category"
-                options={choiceToOptions(choices.service.category)}
-                onChange={(e, data) => {
-                  handleChange(e, data)
-                  setFieldValue('type', '')
-                  setFieldValue('count', 1)
-                }}
-                style={{ marginBottom: '1rem' }}
-              />
-              {errors.category && (
-                <Message error>
-                  <p>{errors.category}</p>
-                </Message>
-              )}
-              {values.category && (
-                <>
-                  <Field
-                    as={Dropdown}
-                    fluid
-                    selection
-                    name="type"
-                    loading={isSubmitting}
-                    placeholder="Select the service type"
-                    options={choiceToOptions(
-                      choices.service['type_' + values.category]
-                    )}
-                    onChange={handleChange}
-                  />
-                  {values.category == 'DISCRETE' ? (
-                    <>
-                      <DateInput
-                        name="started_at"
-                        dateFormat="DD/MM/YYYY"
-                        autoComplete="off"
-                        placeholder="Date"
-                        onChange={handleChange}
-                        value={values.started_at}
-                        style={{ marginTop: '1rem' }}
-                      />
-                      <Input
-                        fluid
-                        name="count"
-                        type="number"
-                        min="1"
-                        placeholder="Count"
-                        onChange={handleChange}
-                        value={values.count}
-                        style={{ marginBottom: '1rem' }}
-                      />
-                    </>
-                  ) : (
-                    <>
-                      <DateInput
-                        name="started_at"
-                        dateFormat="DD/MM/YYYY"
-                        autoComplete="off"
-                        placeholder="Start date"
-                        onChange={handleChange}
-                        value={values.started_at}
-                        style={{ marginTop: '1rem' }}
-                      />
-                      <DateInput
-                        name="finished_at"
-                        dateFormat="DD/MM/YYYY"
-                        autoComplete="off"
-                        placeholder="Finish date"
-                        onChange={handleChange}
-                        value={values.finished_at}
-                      />
-                    </>
-                  )}
-                  <TextArea
-                    disabled={isSubmitting}
-                    rows={2}
-                    placeholder="Add a note"
-                    onChange={(e) => setFieldValue('notes', e.target.value)}
-                    value={values.notes}
-                    style={{ marginBottom: '1rem' }}
-                  />
-                  {Object.entries(errors).map(([k, v]) => (
-                    <Message error key={k}>
-                      <div className="header">{k}</div>
-                      <p>{v}</p>
-                    </Message>
-                  ))}
-                </>
-              )}
+              <FormikServiceFields choices={choices} />
+              <FormikServiceErrorMessages />
               <Button
                 loading={isSubmitting}
-                disabled={isSubmitting}
+                disabled={isSubmitting || !values.category}
                 positive
                 type="submit"
               >
@@ -195,5 +89,161 @@ export const ServiceForm: React.FC<CaseDetailFormProps> = ({
         }}
       </Formik>
     </Segment>
+  )
+}
+
+export const FormikServiceErrorMessages = () => {
+  const { errors } = useFormikContext<ServiceCreate>()
+  return (
+    <>
+      {Object.entries(errors).map(([k, v]) => (
+        <Message error key={k}>
+          <div className="header">{k}</div>
+          <p>{v}</p>
+        </Message>
+      ))}
+    </>
+  )
+}
+
+export const FormikServiceFields = ({
+  choices,
+}: {
+  choices: CaseFormChoices
+}) => {
+  const [category, setCategory] = useState<string>()
+  const { setFieldValue, isSubmitting } = useFormikContext<ServiceCreate>()
+
+  return (
+    <>
+      <Field
+        as={Dropdown}
+        fluid
+        selection
+        name="category"
+        loading={isSubmitting}
+        placeholder="Service category"
+        options={choiceToOptions(choices.service.category)}
+        onChange={(e, data) => {
+          setCategory(data.value)
+          setFieldValue('category', data.value)
+          setFieldValue('type', '')
+          setFieldValue('count', 1)
+        }}
+        style={{ marginBottom: '1rem' }}
+      />
+      {category && (
+        <>
+          {category == ServiceCategory.Discrete ? (
+            <FormikDiscreteServiceFields choices={choices} />
+          ) : (
+            <FormikOngoingServiceFields choices={choices} />
+          )}
+        </>
+      )}
+    </>
+  )
+}
+
+export const FormikDiscreteServiceFields = ({
+  choices,
+}: {
+  choices: CaseFormChoices
+}) => {
+  const { setFieldValue, isSubmitting, values } =
+    useFormikContext<ServiceCreate>()
+  const handleChange = (e, { name, value }) => setFieldValue(name, value, false)
+
+  return (
+    <>
+      <Field
+        as={Dropdown}
+        fluid
+        selection
+        name="type"
+        loading={isSubmitting}
+        placeholder="Service type"
+        options={choiceToOptions(choices.service['type_' + values.category])}
+        onChange={handleChange}
+      />
+      <DateInput
+        name="started_at"
+        dateFormat="DD/MM/YYYY"
+        autoComplete="off"
+        placeholder="Date"
+        onChange={handleChange}
+        value={values.started_at}
+        style={{ marginTop: '1rem' }}
+      />
+      <Input
+        fluid
+        name="count"
+        type="number"
+        min="1"
+        placeholder="Count"
+        onChange={handleChange}
+        value={values.count}
+        style={{ marginBottom: '1rem' }}
+      />
+      <TextArea
+        disabled={isSubmitting}
+        rows={2}
+        placeholder="Notes"
+        onChange={(e) => setFieldValue('notes', e.target.value)}
+        value={values.notes}
+        style={{ marginBottom: '1rem' }}
+      />
+    </>
+  )
+}
+
+export const FormikOngoingServiceFields = ({
+  choices,
+}: {
+  choices: CaseFormChoices
+}) => {
+  const { setFieldValue, isSubmitting, values } =
+    useFormikContext<ServiceCreate>()
+
+  const handleChange = (e, { name, value }) => setFieldValue(name, value, false)
+
+  return (
+    <>
+      <Field
+        as={Dropdown}
+        fluid
+        selection
+        name="type"
+        loading={isSubmitting}
+        placeholder="Service type"
+        options={choiceToOptions(choices.service['type_' + values.category])}
+        onChange={handleChange}
+      />
+      <DateInput
+        name="started_at"
+        dateFormat="DD/MM/YYYY"
+        autoComplete="off"
+        placeholder="Start date"
+        onChange={handleChange}
+        value={values.started_at}
+        style={{ marginTop: '1rem' }}
+      />
+      <DateInput
+        name="finished_at"
+        dateFormat="DD/MM/YYYY"
+        autoComplete="off"
+        placeholder="Finish date"
+        onChange={handleChange}
+        value={values.finished_at}
+      />
+      <TextArea
+        disabled={isSubmitting}
+        rows={2}
+        placeholder="Notes"
+        onChange={(e) => setFieldValue('notes', e.target.value)}
+        value={values.notes}
+        style={{ marginBottom: '1rem' }}
+      />
+    </>
   )
 }
