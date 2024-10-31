f7d0bbb09a88ffd3f02ed78f2f0e491dbe530aaf
Luca Vari <luca.vari@gmail.com>
2024-10-31 16:07:33 +1100

    Don't open clicked links when editing rich text
    It makes the links difficult to edit.

--- frontend/src/comps/rich-text/rich-text-area.tsx
+++ frontend/src/comps/rich-text/rich-text-area.tsx
@@ -33,6 +33,7 @@ export const RichTextArea = ({
         HTMLAttributes: {
           target: null,
         },
+        openOnClick: false,
       }),
       Placeholder.configure({ placeholder: placeholder }),
     ],
