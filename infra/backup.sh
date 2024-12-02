d06073409de2ceb060d923529dee5a671ff36a9c
Luca Vari <luca.vari@gmail.com>
2024-12-02 16:45:06 +1100

    Include "production" in database file backup name to reduce possible confusion
    As we now occasionally backup the staging environment also.

--- infra/backup.sh
+++ infra/backup.sh
@@ -6,13 +6,11 @@
 set -e
 HOST='13.55.250.149'
 TIME=$(date "+%s")
-DATABASE_NAME=clerk
 S3_BUCKET=s3://anika-database-backups
-BACKUP_FILE="postgres_${DATABASE_NAME}_${TIME}.sql.gz"
+BACKUP_FILE="postgres_clerk_production_${TIME}.sql.gz"
 S3_PATH="$S3_BUCKET/$BACKUP_FILE"
 
-if [[ -z "$CLERK_PRIVATE_SSH_KEY" ]]
-then
+if [[ -z "$CLERK_PRIVATE_SSH_KEY" ]]; then
     echo -e "\n>>> Error: Clerk private key not found in CLERK_PRIVATE_SSH_KEY"
     exit 1
 fi
@@ -24,7 +22,7 @@ echo -e "$CLERK_PRIVATE_SSH_KEY" > private.key
 chmod 600 private.key
 
 echo -e "\n>>> SSH into Clerk EC2 instance at $HOST"
-ssh -o StrictHostKeyChecking=no -i private.key root@$HOST /bin/bash << EOF
+ssh -o StrictHostKeyChecking=no -i private.key root@$HOST /bin/bash <<EOF
     set -e
     cd /srv/backups
 
