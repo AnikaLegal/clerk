4e270ee9cdcd88e9bace85760825e73b6ce9967f
Luca Vari <luca.vari@gmail.com>
2025-01-28 11:37:56 +1100

    WIP - Create task when question asked & bunch of refactoring

--- frontend/src/comps/task/modal/task-action-question-modal.tsx
+++ frontend/src/comps/task/modal/task-action-question-modal.tsx
@@ -1,109 +1,149 @@
-import api, { User } from 'api'
-import { EditorEvents, RichTextArea } from 'comps/rich-text'
+import api, { TaskCreate } from 'api'
 import { ModalProps } from 'comps/task/task-action-card'
-import React, { useEffect, useState } from 'react'
-import { Button, Dropdown, Form, Modal } from 'semantic-ui-react'
+import { enqueueSnackbar } from 'notistack'
+import React from 'react'
+import { Button, Form, Modal } from 'semantic-ui-react'
+import { getAPIErrorMessage } from 'utils'
+import { DropdownField, RichTextAreaField } from 'forms/formik'
+import { Formik, FormikHelpers } from 'formik'
+import * as Yup from 'yup'
+
+const QuestionSchema: Yup.ObjectSchema<TaskCreate> = Yup.object({
+  assigned_to_id: Yup.number().required(),
+  description: Yup.string().min(1).required(),
+  issue_id: Yup.string().required(),
+  name: Yup.string().required(),
+  type: Yup.string().required(),
+  /* Below not strictly necessary but prevents type error */
+  due_at: Yup.string().notRequired(),
+  is_approval_required: Yup.boolean().notRequired(),
+  is_approved: Yup.boolean().notRequired(),
+  is_urgent: Yup.boolean().notRequired(),
+  status: Yup.string().notRequired(),
+})
 
 export const QuestionModal = (props: ModalProps) => {
-  const [canSubmit, setCanSubmit] = useState<boolean>(false)
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
-  const [userId, setUserId] = useState<number>()
-  const [text, setText] = useState<string>('')
+  const [createTask] = api.useCreateTaskMutation()
 
-  useEffect(() => {
-    setCanSubmit(text != '')
-  }, [text])
+  const initialValues = {
+    type: 'OTHER',
+    name: `Question from ${props.user.full_name}`,
+    issue_id: props.task.issue.id,
+    assigned_to_id: props.task.issue.lawyer?.id,
+  }
 
-  const assignee = props.task.assigned_to
-  const paralegal = props.task.issue.paralegal
-  const lawyer = props.task.issue.lawyer
-  const currentUser = props.user
+  const handleSubmit = (
+    values: TaskCreate,
+    helpers: FormikHelpers<TaskCreate>
+  ) => {
+    values.description =
+      `${props.user.full_name} asked a question about ` +
+      `<a href="${props.task.url}">this task<a>:` +
+      `<blockquote>${values.description}</blockquote>`
 
-  const userResults = api.useGetUsersQuery({ isActive: true })
-  const users: User[] = [
-    ...(userResults.data || []),
-    ...(lawyer ? [lawyer] : []),
-  ]
-    .filter(
-      ({ id, is_coordinator_or_better }) =>
-        id != currentUser.id &&
-        (id == assignee?.id ||
-          id == paralegal?.id ||
-          id == lawyer?.id ||
-          is_coordinator_or_better)
-    )
-    .sort((a, b) => a.full_name.localeCompare(b.full_name))
-
-  const handleClose = () => {
-    setText('')
+    createTask({ taskCreate: values })
+      .unwrap()
+      .then(() => enqueueSnackbar('Created question', { variant: 'success' }))
+      .catch((e) =>
+        enqueueSnackbar(getAPIErrorMessage(e, 'Failed to create question'), {
+          variant: 'error',
+        })
+      )
     props.onClose()
   }
 
-  const handleSubmit = (e) => {
-    e.stopPropagation()
-    setIsSubmitting(true)
-    setIsSubmitting(false)
-    handleClose()
+  return (
+    <Formik
+      enableReinitialize
+      isInitialValid={false}
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={QuestionSchema}
+    >
+      {(formik) => {
+        const closeHandler = () => {
+          formik.resetForm()
+          props.onClose()
+        }
+
+        return (
+          <Modal size="tiny" open={props.open} onClose={closeHandler}>
+            <Modal.Header>Ask a question about this task</Modal.Header>
+            <Modal.Content>
+              <Form
+                onSubmit={formik.handleSubmit}
+                error={Object.keys(formik.errors).length > 0}
+              >
+                <UserDropdownField {...props} />
+                <RichTextAreaField
+                  name="description"
+                  label="Question"
+                  required
+                />
+              </Form>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button
+                primary
+                type="submit"
+                onClick={() => formik.handleSubmit()}
+                disabled={!formik.isValid}
+              >
+                Send
+              </Button>
+              <Button onClick={closeHandler}>Close</Button>
+            </Modal.Actions>
+          </Modal>
+        )
+      }}
+    </Formik>
+  )
+}
+
+const UserDropdownField = (props: ModalProps) => {
+  const userResults = api.useGetUsersQuery({ isActive: true })
+  const lawyer = props.task.issue.lawyer
+
+  let users = userResults.data ? [...userResults.data] : []
+
+  /* Exclude the current user.
+  /* Exclude the supervising lawyer (for now, we add them back below)
+   * Include only coordinators plus.
+   */
+  users = users.filter(
+    (user) =>
+      user.id != props.user.id &&
+      lawyer &&
+      user.id != lawyer.id &&
+      user.is_coordinator_or_better
+  )
+  /* Sort by full name.
+   */
+  users = users.sort((a, b) => a.full_name.localeCompare(b.full_name))
+
+  /* Add the supervising lawyer back as the first item.
+   */
+  if (
+    lawyer &&
+    !userResults.isLoading &&
+    !users.find((user) => user.id == lawyer.id)
+  ) {
+    users.unshift(lawyer)
   }
 
-  const handleUpdate = ({ editor }: EditorEvents['update']) => {
-    if (editor) {
-      setText(editor.isEmpty || editor.getText() == '' ? '' : editor.getHTML())
-    }
-  }
+  const userOptions = users.map((u) => ({
+    key: u.id,
+    value: u.id,
+    text: u.full_name || u.email,
+    description: (lawyer && u.id === lawyer.id && 'Case Lawyer') || '',
+  }))
 
   return (
-    <Modal
-      as="Form"
-      className="form"
-      open={props.open}
-      onClose={handleClose}
-      onSubmit={(e) => handleSubmit(e)}
-      size="tiny"
-    >
-      <Modal.Header>Ask a question about this task</Modal.Header>
-      <Modal.Content>
-        <Form.Field required>
-          <label>To</label>
-          <Dropdown
-            fluid
-            search
-            selection
-            loading={userResults.isLoading}
-            onChange={(e, { value }) => setUserId(value as number)}
-            openOnFocus={false}
-            value={userId || lawyer?.id || ''}
-            options={
-              users?.map((u) => ({
-                key: u.id,
-                value: u.id,
-                text: u.full_name || u.email,
-                description:
-                  (paralegal && u.id === paralegal.id && 'Paralegal') ||
-                  (lawyer && u.id === lawyer.id && 'Case Lawyer') ||
-                  '',
-              })) || []
-            }
-          />
-        </Form.Field>
-        <Form.Field required>
-          <label>Question</label>
-          <RichTextArea autoFocus onUpdate={handleUpdate} />
-        </Form.Field>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button type="button" onClick={handleClose} disabled={isSubmitting}>
-          Close
-        </Button>
-        <Button
-          primary
-          type="submit"
-          loading={isSubmitting}
-          disabled={isSubmitting || !canSubmit}
-        >
-          Send
-        </Button>
-      </Modal.Actions>
-    </Modal>
+    <DropdownField
+      name="assigned_to_id"
+      label="To"
+      options={userOptions}
+      loading={userResults.isLoading}
+    />
   )
 }
