dc6b19b5ffd849c4d3b9fa078212d81bf67b7f04
Luca Vari <luca.vari@gmail.com>
2025-02-12 14:26:04 +1100

    Rename file to match component name

--- frontend/src/comps/task/modal/index.ts
+++ frontend/src/comps/task/modal/index.ts
@@ -1,6 +1,6 @@
 import { RequestApprovalModal } from './task-action-approval-modal'
 import { CancelTaskModal } from './task-action-cancel-modal'
 import { QuestionModal } from './task-action-question-modal'
-import { CreateTaskModal } from './task-create-modal'
+import { CreateTaskModal } from './create-task-modal'
 
 export { CancelTaskModal, CreateTaskModal, QuestionModal, RequestApprovalModal }
