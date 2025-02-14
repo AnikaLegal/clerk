5a389a6d7cd56ec4c022f98142bb4660fa5317c3
Luca Vari <luca.vari@gmail.com>
2025-02-14 14:59:36 +1100

    Require approval request when attempting to close a task that requires approval
    Also simplify code considerably.

--- frontend/src/comps/task/task-action-card.tsx
+++ frontend/src/comps/task/task-action-card.tsx
@@ -1,10 +1,6 @@
 import { useUpdateTaskStatusMutation, TaskStatusUpdate } from 'api'
 import { TextButton } from 'comps/button'
-import {
-  CancelTaskModal,
-  QuestionModal,
-  RequestApprovalModal,
-} from 'comps/task'
+import { CancelTaskModal, RequestApprovalModal } from 'comps/task'
 import { enqueueSnackbar } from 'notistack'
 import React, { useState } from 'react'
 import { Card, Icon, List, SemanticICONS } from 'semantic-ui-react'
@@ -20,28 +16,28 @@ interface TaskActionProps extends Omit<TaskDetailProps, 'choices'> {
   status: TaskStatus
 }
 
-interface TaskOptionBase {
+interface TaskOption {
   id: string
   icon: SemanticICONS
   text: string
   showWhen: () => boolean
   disableWhen?: () => boolean
-}
-interface TaskActionOption extends TaskOptionBase {
   action: () => void
-  modal?: never
 }
-interface TaskModalOption extends TaskOptionBase {
-  action?: never
-  modal: React.FC<ModalProps>
-}
-type TaskOption = TaskActionOption | TaskModalOption
 
 export const TaskActionCard = (props: TaskActionProps) => {
   const [updateTaskStatus] = useUpdateTaskStatusMutation()
+  const [openCancel, setOpenCancel] = useState(false)
+  const [openApprove, setOpenApprove] = useState(false)
+
+  const task = props.task
+  const status = props.status
+  const user = props.user
+  const isApproved =
+    user.is_lawyer_or_better || !task.is_approval_required || task.is_approved
 
   const updateStatusHandler = (values: TaskStatusUpdate) => {
-    updateTaskStatus({ id: props.task.id, taskStatusUpdate: values })
+    updateTaskStatus({ id: task.id, taskStatusUpdate: values })
       .unwrap()
       .then((task) => {
         props.setTask(task)
@@ -54,16 +50,28 @@ export const TaskActionCard = (props: TaskActionProps) => {
       })
   }
 
-  const perms = props.user
-  const task = props.task
-  const status = props.status
+  const finishTaskHandler = () => {
+    if (isApproved) {
+      updateStatusHandler({ status: status.finished })
+    } else {
+      setOpenApprove(true)
+    }
+  }
 
-  const managementOptions: TaskOption[] = [
+  const cancelTaskHandler = () => {
+    if (isApproved) {
+      setOpenCancel(true)
+    } else {
+      setOpenApprove(true)
+    }
+  }
+
+  const options: TaskOption[] = [
     {
       id: 'reopen',
       icon: 'undo',
       text: 'Reopen the task',
-      showWhen: () => perms.is_paralegal_or_better && !task.is_open,
+      showWhen: () => user.is_paralegal_or_better && !task.is_open,
       action: () => updateStatusHandler({ status: status.stopped }),
     },
     {
@@ -71,7 +79,7 @@ export const TaskActionCard = (props: TaskActionProps) => {
       icon: 'play',
       text: 'Start the task',
       showWhen: () =>
-        perms.is_paralegal_or_better && task.status === status.stopped,
+        user.is_paralegal_or_better && task.status === status.stopped,
       action: () => updateStatusHandler({ status: status.started }),
     },
     {
@@ -79,61 +87,39 @@ export const TaskActionCard = (props: TaskActionProps) => {
       icon: 'stop',
       text: 'Stop the task',
       showWhen: () =>
-        perms.is_paralegal_or_better && task.status === status.started,
+        user.is_paralegal_or_better && task.status === status.started,
       action: () => updateStatusHandler({ status: status.stopped }),
-    },
-    {
-      id: 'approve',
-      icon: 'thumbs up',
-      text: 'Get approval',
-      showWhen: () =>
-        !perms.is_lawyer_or_better &&
-        task.is_open &&
-        task.is_approval_required &&
-        !task.is_approved,
-      modal: RequestApprovalModal,
     },
     {
       id: 'complete',
       icon: 'check',
       text: 'Complete the task',
-      showWhen: () =>
-        perms.is_paralegal_or_better &&
-        task.is_open &&
-        (perms.is_lawyer_or_better ||
-          !task.is_approval_required ||
-          task.is_approved),
-      action: () => updateStatusHandler({ status: status.finished }),
+      showWhen: () => user.is_paralegal_or_better && task.is_open,
+      action: () => finishTaskHandler(),
     },
     {
       id: 'cancel',
       icon: 'close',
       text: 'Cancel the task',
-      showWhen: () =>
-        perms.is_paralegal_or_better &&
-        task.is_open &&
-        (perms.is_lawyer_or_better ||
-          !task.is_approval_required ||
-          task.is_approved),
-      modal: CancelTaskModal,
-    },
-  ]
-
-  const adminOptions: TaskOption[] = [
-    {
-      id: 'question',
-      icon: 'question',
-      text: 'Ask a question',
-      showWhen: () => perms.is_paralegal_or_better && task.is_open,
-      modal: QuestionModal,
+      showWhen: () => user.is_paralegal_or_better && task.is_open,
+      action: () => cancelTaskHandler(),
     },
   ]
 
   return (
     <Card fluid>
+      <CancelTaskModal
+        open={openCancel}
+        onClose={() => setOpenCancel(false)}
+        {...props}
+      />
+      <RequestApprovalModal
+        open={openApprove}
+        onClose={() => setOpenApprove(false)}
+        {...props}
+      />
       <Card.Content header="Task actions" />
-      <TaskActionCardContent options={managementOptions} {...props} />
-      <TaskActionCardContent options={adminOptions} {...props} />
+      <TaskActionCardContent options={options} {...props} />
     </Card>
   )
 }
@@ -142,33 +128,31 @@ interface TaskActionCardContentProps extends TaskActionProps {
   options: TaskOption[]
 }
 export const TaskActionCardContent = (props: TaskActionCardContentProps) => {
-  const visibleOptions = props.options.filter((option) => option.showWhen())
-
-  if (visibleOptions.length < 1) {
+  const options = props.options.filter((option) => option.showWhen())
+  if (options.length < 1) {
     return null
   }
   return (
     <Card.Content>
       <List verticalAlign="middle" selection>
-        {visibleOptions.map((option) =>
-          option.action ? (
-            <TaskAction key={option.id} option={option} />
-          ) : (
-            <TaskModal key={option.id} option={option} {...props} />
-          )
-        )}
+        {options.map((option) => (
+          <TaskAction key={option.id} option={option} />
+        ))}
       </List>
     </Card.Content>
   )
 }
 
 export const TaskAction = ({ option }: { option: TaskOption }) => {
-  const disabled = option.disableWhen && option.disableWhen()
   return (
     <List.Item key={option.id}>
       <List.Content>
         <List.Header>
-          <TextButton icon disabled={disabled} onClick={option.action}>
+          <TextButton
+            icon
+            disabled={option.disableWhen && option.disableWhen()}
+            onClick={option.action}
+          >
             <span>
               <Icon name={option.icon} />
               {option.text}
@@ -177,37 +161,5 @@ export const TaskAction = ({ option }: { option: TaskOption }) => {
         </List.Header>
       </List.Content>
     </List.Item>
-  )
-}
-
-interface TaskModalProps extends TaskActionProps {
-  option: TaskOption
-}
-export const TaskModal = ({ option, ...props }: TaskModalProps) => {
-  const [open, setOpen] = useState(false)
-  const disabled = option.disableWhen && option.disableWhen()
-
-  return (
-    <>
-      <List.Item key={option.id}>
-        <List.Content>
-          <List.Header>
-            <TextButton icon disabled={disabled} onClick={() => setOpen(true)}>
-              <span>
-                <Icon name={option.icon} />
-                {option.text}
-              </span>
-            </TextButton>
-          </List.Header>
-        </List.Content>
-      </List.Item>
-      <option.modal
-        {...props}
-        open={open}
-        onClose={() => {
-          setOpen(false)
-        }}
-      />
-    </>
   )
 }
