38c77e653ea8884730430fff359cd14b9cbd15a1
Luca Vari <luca.vari@gmail.com>
2025-02-20 16:48:42 +1100

    Add task actions for approval requests

--- frontend/src/comps/task/task-approval-action-card.tsx
+++ frontend/src/comps/task/task-approval-action-card.tsx
@@ -1,7 +1,14 @@
-import { TaskStatusUpdate } from 'api'
+import {
+  TaskApprovalUpdate,
+  TaskStatusUpdate,
+  useUpdateTaskApprovalMutation,
+  useUpdateTaskStatusMutation,
+} from 'api'
+import { enqueueSnackbar } from 'notistack'
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 import { TaskDetailProps, TaskStatus } from 'types/task'
+import { getAPIErrorMessage } from 'utils'
 import { TaskActionCardContent, TaskOption } from './task-action-card'
 
 export interface ModalProps extends TaskApprovalActionProps {
@@ -13,23 +20,67 @@ interface TaskApprovalActionProps extends Omit<TaskDetailProps, 'choices'> {
   status: TaskStatus
 }
 
-export const TaskApprovalActionCard = ({ task, ...props }: TaskApprovalActionProps) => {
-  const updateStatusHandler = (values: TaskStatusUpdate) => {}
+export const TaskApprovalActionCard = ({
+  task,
+  setTask,
+  status,
+  user,
+}: TaskApprovalActionProps) => {
+  const [updateTaskApproval] = useUpdateTaskApprovalMutation()
+
+  const updateApprovalHandler = (values: TaskApprovalUpdate) => {
+    updateTaskApproval({
+      id: task.id,
+      taskApprovalUpdate: values,
+    })
+      .unwrap()
+      .then((task) => {
+        setTask(task)
+        enqueueSnackbar('Updated task approval', { variant: 'success' })
+      })
+      .catch((e) => {
+        enqueueSnackbar(
+          getAPIErrorMessage(e, 'Failed to update task approval'),
+          {
+            variant: 'error',
+          }
+        )
+      })
+  }
 
   const options: TaskOption[] = [
+    {
+      id: 'reopen',
+      icon: 'undo',
+      text: 'Reopen the task',
+      showWhen: () => user.is_paralegal_or_better && !task.is_open,
+      action: () =>
+        updateApprovalHandler({
+          status: status.stopped,
+          requesting_task: { is_approved: false },
+        }),
+    },
     {
       id: 'approve',
       icon: 'check',
       text: 'Approve the request',
       showWhen: () => task.is_open,
-      action: () => {},
+      action: () =>
+        updateApprovalHandler({
+          status: status.finished,
+          requesting_task: { is_approved: true },
+        }),
     },
     {
       id: 'deny',
       icon: 'close',
       text: 'Deny the request',
       showWhen: () => task.is_open,
-      action: () => {},
+      action: () =>
+        updateApprovalHandler({
+          status: status.finished,
+          requesting_task: { is_approved: false },
+        }),
     },
   ]
 
