b7fc507600e0332f6bb981523dbd73243cbbafc8
Luca Vari <luca.vari@gmail.com>
2025-03-19 15:04:11 +1100

    Correct Django migration dependency
    The dependency became incorrect after a merged change from the develop branch.

--- app/core/migrations/0076_alter_issueevent_event_type.py
+++ app/core/migrations/0076_alter_issueevent_event_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0074_alter_serviceevent_event_type"),
+        ("core", "0075_alter_tenancy_is_on_lease"),
     ]
 
     operations = [
