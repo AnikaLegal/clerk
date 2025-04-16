87c048cc3f9dffef481af9c23835626437d29e09
Luca Vari <luca.vari@gmail.com>
2025-04-16 12:17:03 +1000

    refactor: change the conflict behaviour when creating a folder using the MS Graph API
    Renaming is inconsistent with our use of paths elsewhere to find specific
    items; if we rename created folders then it will not be at the expected path
    when we look for it later.

--- app/microsoft/endpoints/folder.py
+++ app/microsoft/endpoints/folder.py
@@ -61,7 +61,7 @@ class FolderEndpoint(BaseEndpoint):
 
     def create_folder(self, folder_name, parent_id):
         """
-        Create a folder in the parent
+        Create a folder under the parent folder specified by the supplied id.
         """
         url = f"groups/{settings.MS_GRAPH_GROUP_ID}/drive/items/{parent_id}/children"
         return super().post(
@@ -69,7 +69,7 @@ class FolderEndpoint(BaseEndpoint):
             data={
                 "name": folder_name,
                 "folder": {},  # They do it like this in the docs /shrug
-                "@microsoft.graph.conflictBehavior": "rename",
+                "@microsoft.graph.conflictBehavior": "fail",
             },
         )
 
