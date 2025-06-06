6e2bff6fc555075eff79fe327a3a1e96ee11d81a
Luca Vari <luca.vari@gmail.com>
2025-06-06 16:33:05 +1000

    fix: rename migration file & dependency due to merge

--- app/core/migrations/0083_alter_issue_answers.py
+++ app/core/migrations/0083_alter_issue_answers.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("core", "0079_documenttemplate_initial_data"),
+        ("core", "0082_add_db_collation_natural"),
     ]
 
     operations = [
