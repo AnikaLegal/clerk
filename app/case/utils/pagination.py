f8f93a732bf8416e2fdce562cb742072f01895d4
Luca Vari <luca.vari@gmail.com>
2025-06-23 13:11:58 +1000

    feat(api): allow returning all results from paginated API

--- app/case/utils/pagination.py
+++ app/case/utils/pagination.py
@@ -3,19 +3,35 @@ from rest_framework.response import Response
 
 
 class ClerkPaginator(PageNumberPagination):
+    page_size_query_param = "page_size"
+
+    def paginate_queryset(self, queryset, request, view=None):
+        page_size = request.query_params.get(self.page_size_query_param, self.page_size)
+        if page_size and int(page_size) < 0:
+            return queryset
+        return super().paginate_queryset(queryset, request, view)
+
     def get_paginated_response(self, data):
         next_page_number, prev_page_number = None, None
-        if self.page.has_next():
-            next_page_number = self.page.next_page_number()
+        page_count, current_page_number = 1, 1
+        item_count = len(data)
 
-        if self.page.has_previous():
-            prev_page_number = self.page.previous_page_number()
+        if hasattr(self, "page"):
+            if self.page.has_next():
+                next_page_number = self.page.next_page_number()
+
+            if self.page.has_previous():
+                prev_page_number = self.page.previous_page_number()
+
+            page_count = self.page.paginator.num_pages
+            item_count = self.page.paginator.count
+            current_page_number = self.page.number
 
         return Response(
             {
-                "page_count": self.page.paginator.num_pages,
-                "item_count": self.page.paginator.count,
-                "current": self.page.number,
+                "page_count": page_count,
+                "item_count": item_count,
+                "current": current_page_number,
                 "next": next_page_number,
                 "prev": prev_page_number,
                 "results": data,
