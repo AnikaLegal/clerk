17b7703adcb0789d718509aebbfa609c7d30a149
Luca Vari <luca.vari@gmail.com>
2025-07-14 09:45:25 +1000

    fix: unquote file names in db as we now quote just prior to calling msgraph api

---
+++ app/core/migrations/0085_unquote_document_template_file_names.py
@@ -0,0 +1,28 @@
+from django.db import migrations
+from django_cleanup import cleanup
+from urllib.parse import unquote
+
+
+def _unquote_document_template_file_names(apps, schema_editor):
+    DocumentTemplate = apps.get_model("core", "DocumentTemplate")
+
+    # Prevent django-cleanup from deleting the file because it thinks it has
+    # changed.
+    cleanup.cleanup_ignore(DocumentTemplate)
+
+    for template in DocumentTemplate.objects.all():
+        template.file.name = unquote(template.file.name)
+        template.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("core", "0084_remove_issue_answer_prefix"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            _unquote_document_template_file_names,
+            reverse_code=migrations.RunPython.noop,
+        ),
+    ]
