94caf6285ec7424d5ada3f64f8046c918bd33018
Luca Vari <luca.vari@gmail.com>
2025-07-31 14:59:47 +1000

    build: reload in development when html files are changed

--- app/scripts/web/local.sh
+++ app/scripts/web/local.sh
@@ -4,7 +4,7 @@ watchmedo \
     auto-restart \
     --directory /app/ \
     --recursive \
-    --pattern '*.py' \
+    --pattern '*.py;*.html' \
     --signal SIGHUP \
     -- \
     python -Xfrozen_modules=off manage.py runserver --noreload 0.0.0.0:8000
