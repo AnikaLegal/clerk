25757df26f36c452ae00cb3b9e1d1f2cc1b172ab
Luca Vari <luca.vari@gmail.com>
2025-08-04 14:01:56 +1000

    feat(api): add critical date api & serializer

---
+++ app/case/serializers/issue_date.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from core.models import IssueDate
+from core.models.issue_date import DateType
+from case.serializers.issue import IssueSerializer
+
+
+class IssueDateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IssueDate
+        fields = [
+            "id",
+            "issue",
+            "issue_id",
+            "creator_id",
+            "type",
+            "date",
+            "notes",
+            "is_reviewed",
+        ]
+        extra_kwargs = {
+            "notes": {"required": False},
+            "is_reviewed": {"required": False},
+        }
+
+    issue = IssueSerializer(read_only=True)
+    issue_id = serializers.UUIDField(write_only=True, required=True)
+    type = serializers.ChoiceField(choices=DateType.choices, required=True)
+
+
+class IssueDateSearchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IssueDate
+        fields = (
+            "type",
+            "issue",
+            "is_reviewed",
+        )
+        extra_kwargs = {f: {"required": False} for f in fields}
+
+    issue = serializers.UUIDField()
+    type = serializers.ChoiceField(choices=DateType.choices)
---
+++ app/case/views/date.py
@@ -0,0 +1,79 @@
+from core.models import Issue, IssueDate
+from django.db.models import QuerySet
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+
+from case.serializers.issue_date import IssueDateSearchSerializer, IssueDateSerializer
+from case.utils import render_react_page
+from case.views.auth import (
+    CoordinatorOrBetterPermission,
+    ParalegalOrBetterObjectPermission,
+    coordinator_or_better_required,
+)
+
+
+@api_view(["GET"])
+@coordinator_or_better_required
+def date_list_page_view(request):
+    context = {}
+    return render_react_page(request, "Critical Dates", "date-list", context)
+
+
+class DateApiViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for viewing, creating, updating, and deleting IssueDate objects.
+    """
+
+    serializer_class = IssueDateSerializer
+    permission_classes = [
+        CoordinatorOrBetterPermission | ParalegalOrBetterObjectPermission
+    ]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = IssueDate.objects.all()
+        queryset = queryset.select_related("issue__paralegal")
+
+        if user.is_paralegal:
+            # Paralegals can only see the dates for cases to which they are assigned.
+            queryset = queryset.filter(issue__paralegal=user)
+        elif not user.is_coordinator_or_better:
+            # If you're not a paralegal or coordinator you can't see anything.
+            queryset = queryset.none()
+
+        if self.action == "list":
+            queryset = self.search_queryset(queryset)
+
+        return queryset
+
+    def search_queryset(self, queryset: QuerySet[IssueDate]) -> QuerySet[IssueDate]:
+        """
+        Filter queryset by search terms in query params
+        """
+        serializer = IssueDateSearchSerializer(
+            data=self.request.query_params, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        assert isinstance(serializer.validated_data, dict)  # Keep type checker happy
+
+        search_query = serializer.validated_data
+        for key, value in search_query.items():
+            if value is not None:
+                queryset = queryset.filter(**{key: value})
+
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        issue_id = request.data.get("issue_id")
+        if issue_id:
+            # Check the user has access to the issue before creating the date.
+            try:
+                issue = Issue.objects.get(id=issue_id)
+                self.check_object_permissions(request, issue)
+            except Issue.DoesNotExist:
+                pass  # Handled by serializer validation
+
+        return super().create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        return serializer.save(creator_id=self.request.user.pk)
