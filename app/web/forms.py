4833104feae4312789870a18b52d6a09fb70e713
Luca Vari <luca.vari@gmail.com>
2025-11-19 10:30:03 +1100

    feat(web): add reports to CMS and functionality to track document downloads
    Previously reports were defined statically in HTML - this change allows them to
    be specified in the CMS and adds an optional form before downloading CMS
    documents to collect user data.

--- app/web/forms.py
+++ app/web/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
 from webhooks.models import WebflowContact
-from web.models import ContentFeedback
+from web.models import ContentFeedback, DocumentLog
+from web.models.document import StateChoices, SectorChoices, ReferrerChoices
 
 
 class ContactForm(forms.ModelForm):
@@ -19,3 +20,27 @@ class ContentFeedbackForm(forms.ModelForm):
     class Meta:
         model = ContentFeedback
         fields = ["score", "reason", "name", "email", "page"]
+
+
+class DocumentLogForm(forms.ModelForm):
+    class Meta:
+        model = DocumentLog
+        fields = [
+            "state",
+            "sector",
+            "referrer",
+        ]
+
+    state = forms.ChoiceField(
+        choices=[("", "Where are you located?")] + list(StateChoices.choices),
+        required=True,
+    )
+    sector = forms.ChoiceField(
+        choices=[("", "What sector do you work in?")] + list(SectorChoices.choices),
+        required=True,
+    )
+    referrer = forms.ChoiceField(
+        choices=[("", "Where did you hear about this report?")]
+        + list(ReferrerChoices.choices),
+        required=True,
+    )
