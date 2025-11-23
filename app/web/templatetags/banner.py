05c2be6f3c1e9391209892f9e6cc87a6d4874c1a
Luca Vari <luca.vari@gmail.com>
2025-11-24 09:20:26 +1100

    fix(web): prevent error accessing the request context when getting banner info
    In brief, an error occurs when the banner method is called without a request
    context. The originating cause is not clear - it looks like the db connection
    has been lost - but guarding against it seems like a reasonable course of
    action. See
    https://anika-legal.sentry.io/issues/7049868788/events/38d322325fbb4129872713fa0085035a/.
    
    Also refactor to reduce the number of return statements.

--- app/web/templatetags/banner.py
+++ app/web/templatetags/banner.py
@@ -7,25 +7,23 @@ cookie_prefix = "dismiss_banner"
 
 @register.inclusion_tag("web/snippets/_banner.html", takes_context=True)
 def banner(context):
-    banner = Banner.objects.filter(live=True).last()
-    if not banner:
-        return {}
-
-    # Don't display the banner if:
-    # - The presence of a cookie indicates it has been dismissed already.
-    # - The URL the banner is trying to get us to visit is the one being
-    #   displayed.
-    request = context["request"]
-    cookie_name = f"{cookie_prefix}_{banner.pk}"
-    if (
-        cookie_name in request.COOKIES
-        or banner.call_to_action_url == request.build_absolute_uri()
-    ):
-        return {}
-
-    return {
-        "banner": banner,
-        # Cookie expires in 24 hours.
-        "dismiss_banner_cookie": f"{cookie_name}=true; max-age=86400; Path=/",
-        "request": request,
-    }
+    request = context.get("request")
+    if request:
+        banner = Banner.objects.filter(live=True).last()
+        if banner:
+            # Don't display the banner if:
+            # - The presence of a cookie indicates it has been dismissed already.
+            # - The URL the banner is trying to get us to visit is the one being
+            #   displayed.
+            cookie_name = f"{cookie_prefix}_{banner.pk}"
+            if (
+                cookie_name not in request.COOKIES
+                and banner.call_to_action_url != request.build_absolute_uri()
+            ):
+                return {
+                    "banner": banner,
+                    # Cookie expires in 24 hours.
+                    "dismiss_banner_cookie": f"{cookie_name}=true; max-age=86400; Path=/",
+                    "request": request,
+                }
+    return {}
