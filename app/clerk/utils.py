30cd6e92962b597b364caf3d91e7d69243f9e05c
Luca Vari <luca.vari@gmail.com>
2025-12-04 13:04:48 +1100

    refactor: improve readability for imported symbols with the same name

--- app/clerk/utils.py
+++ app/clerk/utils.py
@@ -1,8 +1,8 @@
-from rest_framework.views import exception_handler
 from case.utils import render_react_page
+from django.core import exceptions as django_exceptions
 from django.http import Http404
-from django.core.exceptions import PermissionDenied
-from rest_framework import exceptions
+from rest_framework import exceptions as drf_exceptions
+from rest_framework.views import exception_handler
 
 
 def custom_exception_handler(exception, context):
@@ -15,12 +15,13 @@ def custom_exception_handler(exception, context):
         and request.path.startswith("/clerk/")
         and not request.path.startswith("/clerk/api/")
     ):
-        if isinstance(exception, PermissionDenied) or isinstance(
-            exception, exceptions.PermissionDenied
+        if isinstance(exception, django_exceptions.PermissionDenied) or isinstance(
+            exception, drf_exceptions.PermissionDenied
         ):
             return render_react_page(request, "Not Allowed", "403", {}, status=403)
-        if isinstance(exception, Http404) or isinstance(exception, exceptions.NotFound):
+        if isinstance(exception, Http404) or isinstance(
+            exception, drf_exceptions.NotFound
+        ):
             return render_react_page(request, "Not Found", "404", {}, status=404)
 
-    response = exception_handler(exception, context)
-    return response
+    return exception_handler(exception, context)
