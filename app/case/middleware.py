7e229df913522af787bcc54cdd1fbe8cf095e80c
Luca Vari <luca.vari@gmail.com>
2025-12-18 09:22:00 +1100

    feat: missed file belonging to previous commit

--- app/case/middleware.py
+++ app/case/middleware.py
@@ -1,31 +1,42 @@
-"""
-https://docs.djangoproject.com/en/3.2/topics/http/middleware/
-"""
-
 from accounts.models import CaseGroups
+
 
 ADMIN_GROUPS = [CaseGroups.ADMIN]
 COORDINATOR_GROUPS = [CaseGroups.ADMIN, CaseGroups.COORDINATOR]
-PARALEGAL_GROUPS = [CaseGroups.ADMIN, CaseGroups.COORDINATOR, CaseGroups.PARALEGAL]
+LAWYER_GROUPS = [CaseGroups.ADMIN, CaseGroups.COORDINATOR, CaseGroups.LAWYER]
+PARALEGAL_GROUPS = [
+    CaseGroups.ADMIN,
+    CaseGroups.COORDINATOR,
+    CaseGroups.LAWYER,
+    CaseGroups.PARALEGAL,
+]
 
 
 def annotate_group_access(user):
-    _is_superuser = user.is_superuser
     group_names = [g.name for g in user.groups.all()]
+
+    _is_superuser = user.is_superuser
     _is_admin_or_better = any([g in ADMIN_GROUPS for g in group_names])
     _is_coordinator_or_better = any([g in COORDINATOR_GROUPS for g in group_names])
+    _is_lawyer_or_better = any([g in LAWYER_GROUPS for g in group_names])
     _is_paralegal_or_better = any([g in PARALEGAL_GROUPS for g in group_names])
+
     # User has permission or higher permission
     user.is_admin_or_better = _is_superuser or _is_admin_or_better
     user.is_coordinator_or_better = _is_superuser or _is_coordinator_or_better
+    user.is_lawyer_or_better = _is_superuser or _is_lawyer_or_better
     user.is_paralegal_or_better = _is_superuser or _is_paralegal_or_better
+
     # User's best permission is this permission
     user.is_admin = CaseGroups.ADMIN in group_names and not _is_superuser
     user.is_coordinator = CaseGroups.COORDINATOR in group_names and not (
         _is_admin_or_better or _is_superuser
     )
-    user.is_paralegal = (CaseGroups.PARALEGAL in group_names) and not (
+    user.is_lawyer = CaseGroups.LAWYER in group_names and not (
         _is_coordinator_or_better or _is_superuser
+    )
+    user.is_paralegal = (CaseGroups.PARALEGAL in group_names) and not (
+        _is_lawyer_or_better or _is_superuser
     )
 
 
