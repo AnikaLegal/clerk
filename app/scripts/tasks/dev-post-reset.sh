919a4320f87143b4cef39e60402186721bd0b92d
Luca Vari <luca.vari@gmail.com>
2026-02-19 11:40:52 +1100

    build: set dev environment admin user names & change password
    Fixes access to the admin user detail page in the frontend as the users full
    name is used to link to the detail page.
    
    Make the password the same as the superusers username so that it is easier to
    remember. NB this is in the dev environment only, no admin user is added in any
    other environment.

--- app/scripts/tasks/dev-post-reset.sh
+++ app/scripts/tasks/dev-post-reset.sh
@@ -5,9 +5,16 @@ set -o errexit
 echo -e "\nRunning migrations"
 ./manage.py migrate
 
-export DJANGO_SUPERUSER_PASSWORD=12345
+export DJANGO_SUPERUSER_PASSWORD="admin"
 echo -e "\nCreating new superuser 'admin' with password: $DJANGO_SUPERUSER_PASSWORD"
 ./manage.py createsuperuser \
     --username admin \
     --email admin@example.com \
     --noinput
+
+SHELL_CMD="space=chr(32);\
+u=User.objects.get(username='admin');\
+u.first_name='Admin';\
+u.last_name='User';\
+u.save()"
+./manage.py shell_plus --quiet-load -c "${SHELL_CMD}"
