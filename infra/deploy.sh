fe2be76bb59b1cdeaf6e9314200b13a6bf4ea121
Luca Vari <luca.vari@gmail.com>
2026-02-25 14:55:33 +1100

    refactor: move hardcoded ip from scripts to env file
    In anticipation that the IP will change shortly.

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -6,8 +6,6 @@
 set -o errexit
 set -o pipefail
 
-HOST='13.55.250.149'
-
 if [[ -z "$COMPOSE_SUFFIX" ]]; then
     echo -e "\n>>> Error: Docker Compose suffix not found in COMPOSE_SUFFIX"
     exit 1
@@ -21,22 +19,28 @@ if [[ -z "$CLERK_PRIVATE_SSH_KEY" ]]; then
     exit 1
 fi
 
-echo -e "\n>>> Deploying $PROJECT to host $HOST with suffix $COMPOSE_SUFFIX"
+CLERK_HOST=$(grep "^CLERK_HOST=" env/${COMPOSE_SUFFIX}.env | cut -d '=' -f2-)
+if [[ -z "$CLERK_HOST" ]]; then
+    echo -e "\n>>> Error: CLERK_HOST not found in env/${COMPOSE_SUFFIX}.env"
+    exit 1
+fi
+
+echo -e "\n>>> Deploying $PROJECT to host $CLERK_HOST with suffix $COMPOSE_SUFFIX"
 
 echo -e "\n>>> Setting up SSH"
 mkdir ~/.ssh
 echo -e "$CLERK_PRIVATE_SSH_KEY" >~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 cat >> ~/.ssh/config <<END
-Host $HOST
+Host $CLERK_HOST
   StrictHostKeyChecking no
 END
 
 echo -e "\n>>> Setting up Docker context"
-docker context create remote --docker "host=ssh://root@${HOST}"
+docker context create remote --docker "host=ssh://root@${CLERK_HOST}"
 docker context use remote
 
-echo -e "\n>>> Deploying $PROJECT to Docker Swarm cluster on host $HOST"
+echo -e "\n>>> Deploying $PROJECT to Docker Swarm cluster on host $CLERK_HOST"
 docker stack deploy --compose-file "docker/docker-compose.${COMPOSE_SUFFIX}.yml" $PROJECT
 
 echo -e "\n>>> Removing unused Docker objects (containers, images, volumes, networks, build cache)"
