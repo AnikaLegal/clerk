77e4b83049c4fce7ee5554cec1f3fae3b70cb721
Luca Vari <luca.vari@gmail.com>
2026-03-19 09:28:51 +1100

    feat(intake): replace HHC landing text with regular intake form text

--- frontend/src/intake/views/LandingView.tsx
+++ frontend/src/intake/views/LandingView.tsx
@@ -1,47 +1,52 @@
 // Client intake for Housing Health Check
 import React from 'react'
-import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-import { Splash, Text, theme } from 'intake/design'
 import { IMAGES, LINKS, ROUTES } from 'intake/consts'
+import { Splash, Text } from 'intake/design'
 import { useScrollTop } from 'intake/utils'
+import { events } from 'intake/analytics'
 
 export const LandingView = () => {
   useScrollTop()
   return (
     <Splash.Container>
       <Splash.Content>
-        <Text.Header splash>Housing Health Check Referrals</Text.Header>
+        <Text.Header splash>
+          Welcome to the Anika Legal intake form!
+        </Text.Header>
         <Text.Body splash>
-          We’re here to help you with your client as they enter their new
-          rental. In order to help you, we need to ask you some questions to see
-          whether they're eligible. This will take about 5 minutes.
+          We’re here to help you with your rental problem. In order for us to
+          help you, we need to ask you a series of simple questions to see
+          whether you're eligible. This questionnaire takes approximately 10
+          minutes to complete.
         </Text.Body>
         <Text.Body splash>
           Before starting the intake form, please have the information ready
           about:
-          <ul>
-            <li>Your client's contact details</li>
-            <li>Your client's PRAP application</li>
-          </ul>
+        </Text.Body>
+        <ul>
+          <li>Your rental property</li>
+          <li>Your rental provider</li>
+          <li>Your agent, if applicable</li>
+          <li>Your income</li>
+        </ul>
+        <Text.Body splash>
           You can have a look at our{' '}
           <a href={LINKS.COLLECTIONS_STATEMENT}>collection statement</a> if you
-          have any questions about why we need this information, and what we do
-          with it.{' '}
+          have any questions about why we need your information, and what we do
+          with it.
         </Text.Body>
-
-        <Text.Body splash>
-          <strong>
-            By proceeding you confirm that you have consent from your client to
-            make this referral and share their personal information.
-          </strong>
-        </Text.Body>
-        <Text.Header splash></Text.Header>
         <Splash.ButtonGroup>
-          <Link to={ROUTES.FORM.replace(':qIdx', '0')}>
+          <Link
+            to={ROUTES.build(ROUTES.FORM, { ':qIdx': 0 }, {})}
+            onClick={events.onStartIntake}
+          >
             <Splash.Button primary>Let’s get started</Splash.Button>
           </Link>
+          <a href={LINKS.SERVICES}>
+            <Splash.Button last>Learn more</Splash.Button>
+          </a>
         </Splash.ButtonGroup>
       </Splash.Content>
       <Splash.Image src={IMAGES.HEROES.PHONE_LADY} />
