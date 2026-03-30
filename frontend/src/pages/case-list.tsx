e5df7363caf8160ce4155a001cfcbe8e1136d64f
Luca Vari <luca.vari@gmail.com>
2026-03-31 10:59:23 +1100

    fix(frontend): handle paginated user API response & update to use Mantine components

--- frontend/src/pages/case-list.tsx
+++ frontend/src/pages/case-list.tsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
 import {
+  Box,
+  Button,
   Container,
-  Input,
-  Form,
+  Grid,
+  Group,
+  Loader,
   Pagination,
-  Icon,
-  Label,
-  Dropdown,
-} from 'semantic-ui-react'
+  Select,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+  Transition,
+} from '@mantine/core'
+import { useDebouncedCallback, useToggle } from '@mantine/hooks'
+import { GetCasesApiArg, useGetCasesQuery } from 'api'
 import { CaseListTable } from 'comps/case-table'
-import { mount } from 'utils'
-import { FadeTransition } from 'comps/transitions'
-import { useGetUsersQuery, useGetCasesQuery, GetCasesApiArg } from 'api'
+import { UserSelect } from 'comps/user-select'
+import React, { useState } from 'react'
 import { UserPermission } from 'types'
-import { Group, Text, Title, Button } from '@mantine/core'
+import { mount } from 'utils'
 
 interface DjangoContext {
   user: UserPermission
@@ -40,32 +46,20 @@ const TABLE_FIELDS = [
 ]
 
 const App = () => {
-  // TODO: Debounce search keystrokes.
-  const [showAdvancedSearch, setShowAdvancedSearch] = useState(false)
-  const [query, setQuery] = useState<GetCasesApiArg>({})
+  const [showAdvancedSearch, toggleAdvancedSearch] = useToggle()
+  const [args, setArgs] = useState<GetCasesApiArg>({})
+  const handleFilterChange = useDebouncedCallback((value: string) => {
+    setArgs((prev) => ({ ...prev, search: value, page: 1 }))
+  }, 300)
 
-  const onPageChange = (e, { activePage }) =>
-    setQuery({ ...query, page: activePage })
-
-  const filteredQuery = Object.entries(query).reduce((obj, [k, v]) => {
-    if (v === null || v === '') return obj
-    return { ...obj, [k]: v }
-  }, {})
-
-  const casesResult = useGetCasesQuery(filteredQuery)
-  const paralegalResults = useGetUsersQuery({ group: 'Paralegal' })
-  const lawyerResults = useGetUsersQuery({ group: 'Lawyer' })
-
+  const casesResult = useGetCasesQuery(args)
   const issues = casesResult.data?.results ?? []
   const totalIssues = casesResult.data?.item_count
   const currentPage = casesResult.data?.current
   const totalPages = casesResult.data?.page_count
-  const isLoading = casesResult.isFetching
 
-  const isLoadingSelections =
-    paralegalResults.isFetching || lawyerResults.isFetching
   return (
-    <Container>
+    <Container size="xl">
       <Title order={1} mb="md">
         <Group wrap="nowrap" gap="sm" justify="space-between">
           <span>Cases</span>
@@ -79,164 +73,136 @@ const App = () => {
           Showing {issues.length} of {totalIssues} cases
         </Text>
       </Title>
-      <Form>
-        <Form.Field>
-          <Input
-            placeholder="Find cases with the name or email of paralegals and clients, or by using the file ref"
-            value={query.search}
-            onChange={(e) => setQuery({ ...query, search: e.target.value })}
-            loading={isLoading}
-          />
-        </Form.Field>
-        {!showAdvancedSearch && (
-          <Label
-            style={{ cursor: 'pointer' }}
-            onClick={(e) => {
-              e.preventDefault()
-              setShowAdvancedSearch(true)
-            }}
-          >
-            Advanced search
-          </Label>
-        )}
-        {showAdvancedSearch && (
-          <>
-            <Label
-              style={{ cursor: 'pointer' }}
-              onClick={(e) => {
-                e.preventDefault()
-                setShowAdvancedSearch(false)
-              }}
-            >
-              Hide advanced search
-            </Label>
-            <Form.Group style={{ marginTop: '1em' }}>
-              <Form.Field width={8}>
-                <Dropdown
-                  fluid
-                  selection
-                  clearable
-                  value={query.isOpen}
-                  placeholder="Is case open?"
-                  options={choiceToOptions(CONTEXT.choices.is_open)}
-                  onChange={(e, { value }) =>
-                    setQuery({ ...query, isOpen: value as string })
-                  }
-                />
-              </Form.Field>
-              <Form.Field width={8}>
-                <Dropdown
-                  fluid
-                  selection
-                  clearable
-                  value={query.stage || ''}
-                  placeholder="Case stage"
-                  options={choiceToOptions(CONTEXT.choices.stage)}
-                  onChange={(e, { value }) =>
-                    setQuery({ ...query, stage: value as string })
-                  }
-                />
-              </Form.Field>
-            </Form.Group>
-            <Form.Group>
-              <Form.Field width={8}>
-                <Dropdown
-                  fluid
-                  selection
-                  clearable
-                  value={query.outcome || ''}
-                  placeholder="Case outcome"
-                  options={choiceToOptions(CONTEXT.choices.outcome)}
-                  onChange={(e, { value }) =>
-                    setQuery({ ...query, outcome: value as string })
-                  }
-                />
-              </Form.Field>
-              <Form.Field width={8}>
-                <Dropdown
-                  fluid
-                  selection
-                  clearable
-                  value={query.topic || ''}
-                  placeholder="Case topic"
-                  options={choiceToOptions(CONTEXT.choices.topic)}
-                  onChange={(e, { value }) =>
-                    setQuery({ ...query, topic: value as string })
-                  }
-                />
-              </Form.Field>
-            </Form.Group>
-            <Form.Group>
-              <Form.Field width={8}>
-                <Dropdown
-                  fluid
-                  selection
-                  search
-                  clearable
-                  value={query.paralegal}
-                  loading={isLoadingSelections}
-                  placeholder="Select a paralegal"
-                  options={paralegalResults.data?.map((u) => ({
-                    key: u.id,
-                    value: u.id,
-                    text: u.email,
-                  }))}
-                  onChange={(e, { value }) =>
-                    setQuery({ ...query, paralegal: value as string })
-                  }
-                />
-              </Form.Field>
-              <Form.Field width={8}>
-                <Dropdown
-                  fluid
-                  selection
-                  search
-                  clearable
-                  value={query.lawyer}
-                  loading={isLoadingSelections}
-                  placeholder="Select a lawyer"
-                  options={lawyerResults.data?.map((u) => ({
-                    key: u.id,
-                    value: u.id,
-                    text: u.email,
-                  }))}
-                  onChange={(e, { value }) =>
-                    setQuery({ ...query, lawyer: value as string })
-                  }
-                />
-              </Form.Field>
-            </Form.Group>
-          </>
-        )}
-      </Form>
-      <FadeTransition in={!isLoading}>
-        <CaseListTable issues={issues} fields={TABLE_FIELDS} />
-      </FadeTransition>
-      {!isLoading && (
-        <Pagination
-          activePage={currentPage}
-          onPageChange={onPageChange as any}
-          totalPages={totalPages}
-          style={{ marginTop: '1em' }}
-          ellipsisItem={{
-            content: <Icon name="ellipsis horizontal" />,
-            icon: true,
-          }}
-          firstItem={{ content: <Icon name="angle double left" />, icon: true }}
-          lastItem={{ content: <Icon name="angle double right" />, icon: true }}
-          prevItem={{ content: <Icon name="angle left" />, icon: true }}
-          nextItem={{ content: <Icon name="angle right" />, icon: true }}
+      <Stack gap="md">
+        <TextInput
+          placeholder="Find cases with the name or email of paralegals and clients, or by using the file ref"
+          onChange={(event) => handleFilterChange(event.currentTarget.value)}
+          rightSection={casesResult.isFetching ? <Loader size="xs" /> : null}
+          size="md"
         />
-      )}
+        <Group>
+          <Button
+            variant="filled"
+            onClick={() => toggleAdvancedSearch()}
+            size="compact-sm"
+            color="gray.3"
+            fw="normal"
+            autoContrast
+          >
+            {showAdvancedSearch ? 'Hide advanced search' : 'Advanced search'}
+          </Button>
+        </Group>
+        {showAdvancedSearch && (
+          <Grid>
+            <Grid.Col span={6}>
+              <Select
+                clearable
+                placeholder="Is case open?"
+                data={choiceToOptions(CONTEXT.choices.is_open)}
+                onChange={(value) =>
+                  setArgs({ ...args, isOpen: value || undefined, page: 1 })
+                }
+                size="md"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Select
+                clearable
+                placeholder="Case stage"
+                data={choiceToOptions(CONTEXT.choices.stage)}
+                onChange={(value) =>
+                  setArgs({ ...args, stage: value || undefined, page: 1 })
+                }
+                size="md"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Select
+                clearable
+                placeholder="Case outcome"
+                data={choiceToOptions(CONTEXT.choices.outcome)}
+                onChange={(value) =>
+                  setArgs({ ...args, outcome: value || undefined, page: 1 })
+                }
+                size="md"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Select
+                clearable
+                placeholder="Case topic"
+                data={choiceToOptions(CONTEXT.choices.topic)}
+                onChange={(value) =>
+                  setArgs({ ...args, topic: value || undefined, page: 1 })
+                }
+                size="md"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <UserSelect
+                searchable
+                clearable
+                placeholder="Select a paralegal"
+                onChange={(value) =>
+                  setArgs({ ...args, paralegal: value || undefined, page: 1 })
+                }
+                params={{
+                  group: 'Paralegal',
+                  sort: 'email',
+                }}
+                size="md"
+              />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <UserSelect
+                searchable
+                clearable
+                placeholder="Select a lawyer"
+                onChange={(value) =>
+                  setArgs({ ...args, lawyer: value || undefined, page: 1 })
+                }
+                params={{
+                  group: 'Lawyer',
+                  sort: 'email',
+                }}
+                size="md"
+              />
+            </Grid.Col>
+          </Grid>
+        )}
+      </Stack>
+      <Box mt="md">
+        <Transition
+          mounted={!casesResult.isFetching}
+          transition="fade"
+          duration={300}
+          timingFunction="ease"
+        >
+          {(styles) => (
+            <Box style={styles}>
+              <CaseListTable issues={issues} fields={TABLE_FIELDS} />
+              {!!totalPages && (
+                <Pagination
+                  total={totalPages}
+                  value={currentPage || 1}
+                  onChange={(page) => setArgs({ ...args, page })}
+                  mt="md"
+                  withEdges
+                  withControls
+                />
+              )}
+            </Box>
+          )}
+        </Transition>
+      </Box>
     </Container>
   )
 }
 
-const choiceToOptions = (choices) =>
+const choiceToOptions = (choices: string[][]) =>
   choices.map(([value, label]) => ({
-    key: label,
-    text: label,
-    value: value,
+    value,
+    label,
   }))
 
 mount(App)
