381e8560659c2bed46ad60ad130072d16e7000a3
Luca Vari <luca.vari@gmail.com>
2026-03-31 11:39:57 +1100

    fix(frontend): minor adjustments to case list for consistency & reuse components

--- frontend/src/pages/case-list.tsx
+++ frontend/src/pages/case-list.tsx
@@ -4,18 +4,16 @@ import {
   Container,
   Grid,
   Group,
-  Loader,
   Pagination,
-  Select,
   Stack,
   Text,
-  TextInput,
   Title,
   Transition,
 } from '@mantine/core'
-import { useDebouncedCallback, useToggle } from '@mantine/hooks'
+import { useToggle } from '@mantine/hooks'
 import { GetCasesApiArg, useGetCasesQuery } from 'api'
 import { CaseListTable } from 'comps/case-table'
+import { SelectFilter, TextInputFilter } from 'comps/filter'
 import { UserSelect } from 'comps/user-select'
 import React, { useState } from 'react'
 import { UserPermission } from 'types'
@@ -48,19 +46,23 @@ const TABLE_FIELDS = [
 const App = () => {
   const [showAdvancedSearch, toggleAdvancedSearch] = useToggle()
   const [args, setArgs] = useState<GetCasesApiArg>({})
-  const handleFilterChange = useDebouncedCallback((value: string) => {
-    setArgs((prev) => ({ ...prev, search: value, page: 1 }))
-  }, 300)
 
-  const casesResult = useGetCasesQuery(args)
-  const issues = casesResult.data?.results ?? []
-  const totalIssues = casesResult.data?.item_count
-  const currentPage = casesResult.data?.current
-  const totalPages = casesResult.data?.page_count
+  const handleFilterChange = (key, value) => {
+    setArgs((prev) => ({
+      ...prev,
+      [key]: value !== null ? value : undefined,
+      page: 1, // Reset to first page on filter change
+    }))
+  }
+
+  const result = useGetCasesQuery(args)
+  const issues = result.data?.results ?? []
+  const currentPage = result.data?.current
+  const totalPages = result.data?.page_count
 
   return (
     <Container size="xl">
-      <Title order={1} mb="md">
+      <Title order={1}>
         <Group wrap="nowrap" gap="sm" justify="space-between">
           <span>Cases</span>
           {CONTEXT.user.is_coordinator_or_better && (
@@ -69,16 +71,17 @@ const App = () => {
             </Button>
           )}
         </Group>
-        <Text className="subtitle">
-          Showing {issues.length} of {totalIssues} cases
-        </Text>
       </Title>
-      <Stack gap="md">
-        <TextInput
+      {!result.isLoading && result.data && (
+        <Text c="dimmed">
+          Showing {result.data.results.length} of {result.data.item_count} cases
+        </Text>
+      )}
+      <Stack gap="md" mt="md">
+        <TextInputFilter
+          name="search"
           placeholder="Find cases with the name or email of paralegals and clients, or by using the file ref"
-          onChange={(event) => handleFilterChange(event.currentTarget.value)}
-          rightSection={casesResult.isFetching ? <Loader size="xs" /> : null}
-          size="md"
+          onFilterChange={handleFilterChange}
         />
         <Group>
           <Button
@@ -95,47 +98,35 @@ const App = () => {
         {showAdvancedSearch && (
           <Grid>
             <Grid.Col span={6}>
-              <Select
-                clearable
+              <SelectFilter
+                name="isOpen"
                 placeholder="Is case open?"
                 data={choiceToOptions(CONTEXT.choices.is_open)}
-                onChange={(value) =>
-                  setArgs({ ...args, isOpen: value || undefined, page: 1 })
-                }
-                size="md"
+                onFilterChange={handleFilterChange}
               />
             </Grid.Col>
             <Grid.Col span={6}>
-              <Select
-                clearable
+              <SelectFilter
+                name="stage"
                 placeholder="Case stage"
                 data={choiceToOptions(CONTEXT.choices.stage)}
-                onChange={(value) =>
-                  setArgs({ ...args, stage: value || undefined, page: 1 })
-                }
-                size="md"
+                onFilterChange={handleFilterChange}
               />
             </Grid.Col>
             <Grid.Col span={6}>
-              <Select
-                clearable
+              <SelectFilter
+                name="outcome"
                 placeholder="Case outcome"
                 data={choiceToOptions(CONTEXT.choices.outcome)}
-                onChange={(value) =>
-                  setArgs({ ...args, outcome: value || undefined, page: 1 })
-                }
-                size="md"
+                onFilterChange={handleFilterChange}
               />
             </Grid.Col>
             <Grid.Col span={6}>
-              <Select
-                clearable
+              <SelectFilter
+                name="topic"
                 placeholder="Case topic"
                 data={choiceToOptions(CONTEXT.choices.topic)}
-                onChange={(value) =>
-                  setArgs({ ...args, topic: value || undefined, page: 1 })
-                }
-                size="md"
+                onFilterChange={handleFilterChange}
               />
             </Grid.Col>
             <Grid.Col span={6}>
@@ -173,7 +164,7 @@ const App = () => {
       </Stack>
       <Box mt="md">
         <Transition
-          mounted={!casesResult.isFetching}
+          mounted={!result.isFetching}
           transition="fade"
           duration={300}
           timingFunction="ease"
